ce4bb4df59d375eb265a9d8f6cfde397b9480271
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-04-22 17:28:34 +0100

    function plot and outputs

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -2,11 +2,61 @@
 
 function(input, output) {
   
-  plot_trend_chart <- function(min = 500, max = 1000, color_chosen = "blue") {
+  # For debugging
+  observeEvent(input$browser, browser())
+  
+  # Reactive controls for areaname depending on areatype selected
+  output$geoname_ui <- renderUI({
     
-    trend_data <- data.frame(date_event = seq(as.Date('2020-01-01'), as.Date('2020-03-31'), by = 'day'),
-                             value = runif(91, min, max)) %>% 
-      filter(between(date_event, input$time_media[1], input$time_media[2]))
+    areas_summary <- sort(geo_lookup$areaname[geo_lookup$areatype == input$geotype_summary])
+    
+    selectizeInput("geoname", label = NULL,  
+                   choices = areas_summary, selected = "")
+    
+  })
+
+  
+  
+  
+  plot_trend_chart <- function(pal_chose, type) {
+    
+    if (type == "sex") {
+      trend_data <- rbind(data.frame(date_event = seq(as.Date('2020-01-01'), as.Date('2020-03-31'), by = 'day'),
+                               value = runif(91, 1000, 1200), type = "All"),
+                          data.frame(date_event = seq(as.Date('2020-01-01'), as.Date('2020-03-31'), by = 'day'),
+                                     value = runif(91, 500, 700), type = "Female"),
+                          data.frame(date_event = seq(as.Date('2020-01-01'), as.Date('2020-03-31'), by = 'day'),
+                                     value = runif(91, 700, 900), type = "Male")) #%>% 
+       # filter(between(date_event, input$time_media[1], input$time_media[2]))
+    } else if (type == "age") {
+      trend_data <- rbind(data.frame(date_event = seq(as.Date('2020-01-01'), as.Date('2020-03-31'), by = 'day'),
+                                     value = runif(91, 300, 400), type = "0-10"),
+                          data.frame(date_event = seq(as.Date('2020-01-01'), as.Date('2020-03-31'), by = 'day'),
+                                     value = runif(91, 400, 500), type = "11-20"),
+                          data.frame(date_event = seq(as.Date('2020-01-01'), as.Date('2020-03-31'), by = 'day'),
+                                     value = runif(91, 500, 600), type = "21-30"),
+                          data.frame(date_event = seq(as.Date('2020-01-01'), as.Date('2020-03-31'), by = 'day'),
+                                     value = runif(91, 600, 700), type = "31-40"),
+                          data.frame(date_event = seq(as.Date('2020-01-01'), as.Date('2020-03-31'), by = 'day'),
+                                     value = runif(91, 700, 800), type = "41-50"),
+                          data.frame(date_event = seq(as.Date('2020-01-01'), as.Date('2020-03-31'), by = 'day'),
+                                     value = runif(91, 800, 900), type = "51-65"),
+                          data.frame(date_event = seq(as.Date('2020-01-01'), as.Date('2020-03-31'), by = 'day'),
+                                     value = runif(91, 900, 1000), type = "+75")) %>% 
+        filter(between(date_event, input$time_media[1], input$time_media[2]))
+    } else if (type == "depr") {
+      trend_data <- rbind(data.frame(date_event = seq(as.Date('2020-01-01'), as.Date('2020-03-31'), by = 'day'),
+                                     value = runif(91, 900, 1000), type = "1 - Most deprived"),
+                          data.frame(date_event = seq(as.Date('2020-01-01'), as.Date('2020-03-31'), by = 'day'),
+                                     value = runif(91, 800, 900), type = "2"),
+                          data.frame(date_event = seq(as.Date('2020-01-01'), as.Date('2020-03-31'), by = 'day'),
+                                     value = runif(91, 700, 800), type = "3"),
+                          data.frame(date_event = seq(as.Date('2020-01-01'), as.Date('2020-03-31'), by = 'day'),
+                                     value = runif(91, 600, 700), type = "4"),
+                          data.frame(date_event = seq(as.Date('2020-01-01'), as.Date('2020-03-31'), by = 'day'),
+                                     value = runif(91, 500, 600), type = "5 - least deprived")) %>% 
+        filter(between(date_event, input$time_media[1], input$time_media[2]))
+    }
     
     xaxis_plots <- list(title = FALSE, tickfont = list(size=14), titlefont = list(size=14), 
                         showline = TRUE, tickangle = 270, fixedrange=TRUE)
@@ -15,10 +65,9 @@ function(input, output) {
                         tickfont = list(size=14), titlefont = list(size=14)) 
     
     #Creating time trend plot
-    trend_plot <- plot_ly(data=trend_data, x=~date_event,  y = ~value) %>% 
+    plot_ly(data=trend_data, x=~date_event,  y = ~value) %>% 
       add_trace(type = 'scatter', mode = 'lines+markers', 
-                marker = list(size = 8, color = color_chosen), 
-                line = list(color = color_chosen)) %>% 
+                color = ~type, colors = pal_chose) %>% 
       #Layout 
       layout(margin = list(b = 160, t=5), #to avoid labels getting cut out
              yaxis = yaxis_plots, xaxis = xaxis_plots) %>%  
@@ -26,67 +75,29 @@ function(input, output) {
     
   }
   
-  output$hits_media_plot <- renderPlotly(plot_trend_chart())
-  output$sentiment_plot <- renderPlotly(plot_trend_chart(0, 1))
-  output$nhs24_plot <- renderPlotly(plot_trend_chart(color_chosen = "green"))
-  output$aye_plot <- renderPlotly(plot_trend_chart(color_chosen = "green"))
-  output$coronacases_plot <- renderPlotly(plot_trend_chart(color_chosen = "orange"))
-  output$coronadeaths_plot <- renderPlotly(plot_trend_chart(color_chosen = "orange"))
-  output$hits_hps_plot <- renderPlotly(plot_trend_chart(color_chosen = "green"))
-  output$retweets_plot <- renderPlotly(plot_trend_chart(color_chosen = "orange"))
-  output$word_cloud_plot <- renderPlotly({
+  output$aye_sex <- renderPlotly(plot_trend_chart(pal_sex, "sex"))
+  output$aye_age <- renderPlotly(plot_trend_chart(pal_age, "age"))
+  output$aye_depr <- renderPlotly(plot_trend_chart(pal_depr, "depr"))
+  output$ooh_sex <- renderPlotly(plot_trend_chart(pal_sex, "sex"))
+  output$ooh_age <- renderPlotly(plot_trend_chart(pal_age, "age"))
+  output$ooh_depr <- renderPlotly(plot_trend_chart(pal_depr, "depr"))
+  output$adm_sex <- renderPlotly(plot_trend_chart(pal_sex, "sex"))
+  output$adm_age <- renderPlotly(plot_trend_chart(pal_age, "age"))
+  output$adm_depr <- renderPlotly(plot_trend_chart(pal_depr, "depr"))
+  output$disch_sex <- renderPlotly(plot_trend_chart(pal_sex, "sex"))
+  output$disch_age <- renderPlotly(plot_trend_chart(pal_age, "age"))
+  output$disch_depr <- renderPlotly(plot_trend_chart(pal_depr, "depr"))
+  output$test_sex <- renderPlotly(plot_trend_chart(pal_sex, "sex"))
+  output$test_age <- renderPlotly(plot_trend_chart(pal_age, "age"))
+  output$test_depr <- renderPlotly(plot_trend_chart(pal_depr, "depr"))
+  output$nhs24_sex <- renderPlotly(plot_trend_chart(pal_sex, "sex"))
+  output$nhs24_age <- renderPlotly(plot_trend_chart(pal_age, "age"))
+  output$nhs24_depr <- renderPlotly(plot_trend_chart(pal_depr, "depr"))
     
-    trend_data <- data.frame(word = c("Flu", "UK", "Crisis", "Outbreak", "Panic",
-                                      "Calm", "Health", "Sneeze", "Caution", "Cases"),
-                             value = exp(seq(1, 5.5, by = 0.5)))
-    
-    xaxis_plots <- list(title = FALSE, tickfont = list(size=14), titlefont = list(size=14), 
-                        showline = TRUE, tickangle = 270, fixedrange=TRUE,
-                        categoryorder = "array", categoryarray = sort(trend_data[,"value"]))
-    
-    yaxis_plots <- list(title = FALSE, rangemode="tozero", fixedrange=TRUE, size = 4, 
-                        tickfont = list(size=14), titlefont = list(size=14)) 
-    
-    
-    plot_ly(data=trend_data, x=~word, y=~value,
-            type = "bar", marker = list(color = "blue")) %>% 
-      #Layout 
-      layout(margin = list(b = 160, t=5), #to avoid labels getting cut out
-             yaxis = yaxis_plots, xaxis = xaxis_plots) %>%  
-      config(displayModeBar = FALSE, displaylogo = F) # taking out plotly logo button
-    
-  })
-  
-  output$correlation_plot <- renderPlotly({
-    
-    trend_data <- data.frame(ind1 = runif(91, 500, 1000),
-                             ind2 = runif(91, 400, 700),
-                             date_event = seq(as.Date('2020-01-01'), as.Date('2020-03-31'), by = 'day'))
-    
-    xaxis_plots <- list(title = FALSE, tickfont = list(size=14), titlefont = list(size=14), 
-                        showline = TRUE, tickangle = 270, fixedrange=TRUE)
-    
-    yaxis_plots <- list(title = FALSE, rangemode="tozero", fixedrange=TRUE, size = 4, 
-                        tickfont = list(size=14), titlefont = list(size=14)) 
-    
-    fit <- lm(ind2 ~ ind1, data = trend_data)
-    
-    
-    plot_ly(data=trend_data) %>% 
-      add_markers(x=~ind1, y=~ind2,
-                  type = "scatter", marker = list(color = "blue")) %>% 
-      add_lines(x=~ind1, y = fitted(fit), mode = "lines") %>%
-      #Layout 
-      layout(margin = list(b = 160, t=5), #to avoid labels getting cut out
-             yaxis = yaxis_plots, xaxis = xaxis_plots) %>%  
-      config(displayModeBar = FALSE, displaylogo = F) # taking out plotly logo button
-    
-  })
-  
   
   table_data <- data.frame(date_event = seq(as.Date('2020-01-02'), as.Date('2020-03-31'), by = 'day'),
                            value = runif(90, 500, 1000),
-                           measure = rep(measure_list, 10)) %>% 
+                           measure = rep(measure_list, 15)) %>% 
     mutate(value = round(value, 0))
   
   output$table_filtered <- DT::renderDataTable({
