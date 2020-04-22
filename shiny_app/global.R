c25a3a3558d421b714bfba9a51318edae50a0cf1
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-04-22 17:28:12 +0100

    palettes, measures

--- shiny_app/global.R
+++ shiny_app/global.R
@@ -20,10 +20,24 @@ plot_box <- function(title_plot, plot_output) {
       plotlyOutput(plot_output))
 }
 
-measure_list <- c("Visits to selected media", "Retweets/favourites",
-                  "Visits to coronavirus pages in HPS", "Sentiment analysis",
-                  "Positive cases of coronavirus", "Calls to NHS 24", 
-                  "Most used word", "Deaths attributed to coronavirus",
+###############################################.
+## Data ----
+###############################################.
+geo_lookup <- readRDS("data/geo_lookup.rds")
+
+measure_list <- c("OOH calls", "Testing",
+                  "Admissions", "Discharges", "Calls to NHS 24", 
                   "A&E attendances")
+
+###############################################.
+## Palettes ----
+###############################################.
+pal_depr <- c('#abd9e9', '#74add1', '#4575b4', '#313695', '#022031')
+#Palette for 9 series in a gradient
+pal_age <- c('#543005', '#8c510a', '#bf812d', '#dfc27d', '#d0d1e6',
+                   '#abd9e9', '#74add1', '#4575b4', '#313695')
+#Palette for those with a single category per sex and overall
+pal_sex <- c('#000000', '#08519c','#bdd7e7')
+
 
 ## END
