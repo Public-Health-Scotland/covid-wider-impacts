586bf58e45f1fc077577ae6b22cc42ae071ecced
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-04-24 12:42:40 +0100

    plot parameters

--- shiny_app/global.R
+++ shiny_app/global.R
@@ -29,7 +29,7 @@ rapid <- readRDS("data/rapid_data.rds") #RAPID data
 spec_list <- sort(unique(rapid$spec)) # specialty list
 
 ###############################################.
-## Palettes ----
+## Palettes and plot parameters ----
 ###############################################.
 pal_depr <- c('#abd9e9', '#74add1', '#4575b4', '#313695', '#022031')
 #Palette for 9 series in a gradient
@@ -38,5 +38,11 @@ pal_age <- c('#543005', '#8c510a', '#bf812d', '#dfc27d', '#d0d1e6',
 #Palette for those with a single category per sex and overall
 pal_sex <- c('#000000', '#08519c','#bdd7e7')
 
+# Style of x and y axis
+xaxis_plots <- list(title = FALSE, tickfont = list(size=14), titlefont = list(size=14),
+                    showline = TRUE, tickangle = 270, fixedrange=TRUE)
+
+yaxis_plots <- list(title = FALSE, rangemode="tozero", fixedrange=TRUE, size = 4,
+                    tickfont = list(size=14), titlefont = list(size=14))
 
 ## END
