859964fdd37d7641892c139228198b1edf196029
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-04-27 10:02:21 +0100

    age sex chart

--- pub_charts_tables.R
+++ pub_charts_tables.R
@@ -32,32 +32,6 @@ plot_subtitle <- "12 January 2020 - 21 April 2020"
 ## RAPID charts and tables ----
 ###############################################.
 ###############################################.
-# Table
-# Table for sex and age
-rap_table <- readRDS("/conf/PHSCOVID19_Analysis/Admissions_by_category_24_Apr.rds") %>% 
-  janitor::clean_names() %>% 
-  # taking out aggregated values, not clear right now
-  filter(!(substr(hosp,3,5) == "All" | (substr(hscp_name,3,5) == "All")))
-
-rap_table <- rap_table %>% 
-  filter(between(date_adm, as.Date("2020-01-12"), as.Date("2020-04-21"))) %>% 
-  group_by(age_group, sex) %>% 
-  summarise(count= sum(count)) %>% ungroup ()
-
-rap_table <- rap_table %>% ungroup() %>% 
-  mutate(age_group = recode(age_group, "5_thru_14" = "5 - 14", "15_thru_44" = "15 - 44", "45_thru_64" = "45 - 64",
-                           "65_thru_74" = "65 -74", "75_thru_84" = "75 -84",
-                           "85+" = "Over 85", "Under_5" = "Under 5")) %>% 
-  mutate(age_group = factor(age_group, levels = c("Under 5", "5 - 14", "15 - 44", "45 - 64", "65 -74", 
-                                                "75 -84", "Over 85"))) %>%
-  mutate(sex = recode(sex, "male" = "Male", "female" = "Female", .missing = "Unknown")) %>% 
-  pivot_wider(names_from = sex, values_from = count) %>% 
-  arrange(age_group) %>% 
-  rename("Age group" = age_group)
-
-write_csv(rap_table, paste0(outputs, "rapid_agesex_table.csv"))
-
-###############################################.
 # Specialty bullet point 
 # What specialty got most admissions
 rap_spec_bullet <- rap_pub %>% 
@@ -69,23 +43,69 @@ rap_spec_bullet <- rap_pub %>%
 write_csv(rap_spec_bullet, paste0(outputs, "rapid_spec_table.csv"))
 
 ###############################################.
-# Chart for age groups
-rap_age_chart_data <- rap_pub %>% 
-  filter(spec == "All" & type == "age" & admission_type == "All" &
-           between(date, as.Date("2020-01-12"), as.Date("2020-04-21"))) %>% 
-  mutate(category = factor(category, levels = c("Under 5", "5 - 14", "15 - 44", "45 - 64", "65 -74", 
-                                                "75 -84", "Over 85"))) %>% 
-  group_by(category) %>% summarise(count = sum(count))
+# Chart and table for age and sex groups
+.
+# Preparing the data
+rap_agesex <- readRDS("/conf/PHSCOVID19_Analysis/Admissions_by_category_24_Apr.rds") %>% 
+  janitor::clean_names() %>% 
+  # taking out aggregated values, not clear right now
+  filter(!(substr(hosp,3,5) == "All" | (substr(hscp_name,3,5) == "All")))
 
-rap_age_chart <- ggplot(rap_age_chart_data, aes(x=category, y = count))+
-  geom_bar(stat = "identity", fill = "#004785")+
+rap_agesex <- rap_agesex %>% 
+  filter(between(date_adm, as.Date("2020-01-12"), as.Date("2020-04-21"))) %>% 
+  group_by(age_group, sex) %>% 
+  summarise(count= sum(count)) %>% ungroup () %>% 
+  mutate(age_group = recode(age_group, "5_thru_14" = "5 - 14", "15_thru_44" = "15 - 44", "45_thru_64" = "45 - 64",
+                            "65_thru_74" = "65 -74", "75_thru_84" = "75 -84",
+                            "85+" = "85 and over", "Under_5" = "Under 5")) %>% 
+  mutate(age_group = factor(age_group, levels = c("Under 5", "5 - 14", "15 - 44", "45 - 64", "65 -74", 
+                                                  "75 -84", "85 and over"))) %>%
+  mutate(sex = recode(sex, "male" = "Male", "female" = "Female", .missing = "Unknown")) 
+
+rap_table <- rap_agesex %>% 
+  pivot_wider(names_from = sex, values_from = count) %>% 
+  arrange(age_group) %>% 
+  rename("Age group" = age_group)
+
+write_csv(rap_table, paste0(outputs, "rapid_agesex_table.csv"))
+
+#Bringing population to calculate rates
+pop_lookup <- readRDS("/conf/linkage/output/lookups/Unicode/Populations/Estimates/HB2019_pop_est_1981_2018.rds") %>% 
+  janitor::clean_names() %>% 
+  filter(year == 2018) %>% 
+  mutate(sex = recode(sex_name, "M" = "Male", "F" = "Female"),
+         age_group = case_when(age < 5 ~ "Under 5", age>=5 & age<15 ~ "5 - 14", 
+                               age>=15 & age<45 ~ "15 - 44", age>=45 & age<65 ~ "45 - 64", 
+                               age>=65 & age<75 ~ "65 -74", 
+                               age>=75 & age<85 ~ "75 -84", age>=85 ~ "85 and over")) %>% 
+  group_by(sex, age_group) %>% 
+  summarise(pop = sum(pop))
+
+rap_agesex <- rap_agesex %>% filter(sex != "Unknown")
+
+rap_agesex <- left_join(rap_agesex, pop_lookup, by = c("sex", "age_group"))
+# Rates per 100,000 people
+rap_agesex <- rap_agesex %>% mutate(rate = round((count/pop)*100000)) %>% 
+  mutate(age_group = factor(age_group, levels = c("Under 5", "5 - 14", "15 - 44", "45 - 64", "65 -74", 
+                                                  "75 -84", "85 and over"))) 
+
+write_csv(rap_agesex, paste0(outputs, "rapid_agesex_chart_data.csv"))
+
+rap_age_chart <- ggplot(rap_agesex, aes(x=age_group, y = ifelse(sex == "Male", -rate, rate),
+                        fill = sex)) +
+  geom_bar(stat = "identity") +
+  scale_fill_manual(values = c("#004785", "#bdd7e7")) +
   # Axis, plot  and legend titles
-  labs(y = "Number of admissions", x = "Age group",
-       title = "Number of admissions to hospital in Scotland by age group",
-       subtitle = plot_subtitle)+
+  labs(y = "Age group", x = "Admissions to hospital per 100,000 people",
+       title = "Rate of admissions to hospital in Scotland by age group and sex",
+       subtitle = plot_subtitle, fill = "Sex")+
+  scale_y_continuous(labels = abs, limits = max(rap_agesex$rate) * c(-1,1)) +
   theme_classic() +
   theme(axis.text = element_text(colour = "black")) +
-  scale_y_continuous(expand = c(0, 0), limits = c(0, 10000))
+  coord_flip()
+
+# +
+#   scale_y_continuous(expand = c(0, 0), limits = c(0, 10000))
 
 rap_age_chart
 
