fec13e931525da3ed91a47da02a2d8b1b3ed7f55
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-04-27 09:24:40 +0100

    saving chart data, longer time trends

--- pub_charts_tables.R
+++ pub_charts_tables.R
@@ -26,7 +26,7 @@ pal_age <- c('#543005', '#8c510a', '#bf812d',  '#d0d1e6',
 pal_depr <- c('#abd9e9', '#74add1', '#4575b4', '#313695', '#022031')
 pal_overall <- c('#000000', '#08519c','#bdd7e7')
 
-plot_subtitle <- "22 March 2020 - 21 April 2020" 
+plot_subtitle <- "12 January 2020 - 21 April 2020" 
 
 ###############################################.
 ## RAPID charts and tables ----
@@ -40,7 +40,7 @@ rap_table <- readRDS("/conf/PHSCOVID19_Analysis/Admissions_by_category_24_Apr.rd
   filter(!(substr(hosp,3,5) == "All" | (substr(hscp_name,3,5) == "All")))
 
 rap_table <- rap_table %>% 
-  filter(between(date_adm, as.Date("2020-03-22"), as.Date("2020-04-21"))) %>% 
+  filter(between(date_adm, as.Date("2020-01-12"), as.Date("2020-04-21"))) %>% 
   group_by(age_group, sex) %>% 
   summarise(count= sum(count)) %>% ungroup ()
 
@@ -62,7 +62,7 @@ write_csv(rap_table, paste0(outputs, "rapid_agesex_table.csv"))
 # What specialty got most admissions
 rap_spec_bullet <- rap_pub %>% 
   filter(type == "sex" & admission_type == "All" & category == "All" &
-           between(date, as.Date("2020-03-22"), as.Date("2020-04-21"))) %>% 
+           between(date, as.Date("2020-01-12"), as.Date("2020-04-21"))) %>% 
   group_by(spec) %>% summarise(count = sum(count)) %>% 
   arrange(-count)
 
@@ -72,7 +72,7 @@ write_csv(rap_spec_bullet, paste0(outputs, "rapid_spec_table.csv"))
 # Chart for age groups
 rap_age_chart_data <- rap_pub %>% 
   filter(spec == "All" & type == "age" & admission_type == "All" &
-           between(date, as.Date("2020-03-22"), as.Date("2020-04-21"))) %>% 
+           between(date, as.Date("2020-01-12"), as.Date("2020-04-21"))) %>% 
   mutate(category = factor(category, levels = c("Under 5", "5 - 14", "15 - 44", "45 - 64", "65 -74", 
                                                 "75 -84", "Over 85"))) %>% 
   group_by(category) %>% summarise(count = sum(count))
@@ -95,7 +95,15 @@ ggsave(paste0(outputs, "rapid_age_chart.png"), rap_age_chart)
 # Chart for overall/elective/emergency 
 rap_overall_chart_data <- rap_pub %>% 
   filter(spec == "All" & type == "sex" & category == "All" &
-           between(date, as.Date("2020-03-22"), as.Date("2020-04-21"))) 
+           between(date, as.Date("2020-01-12"), as.Date("2020-04-21"))) 
+
+write_csv(rap_overall_chart_data, paste0(outputs, "rap_admissions_chart_data.csv"))
+
+rap_overall_2019_chart_data <- rap_pub %>% 
+  filter(spec == "All" & type == "sex" & category == "All" &
+           between(date, as.Date("2019-01-12"), as.Date("2019-04-21"))) 
+
+write_csv(rap_overall_2019_chart_data, paste0(outputs, "rap_admissions_2019_chart_data.csv"))
 
 rap_overall_chart <- ggplot(rap_overall_chart_data, aes(x=date, y = count, color = admission_type))+
   geom_line()+
@@ -106,11 +114,27 @@ rap_overall_chart <- ggplot(rap_overall_chart_data, aes(x=date, y = count, color
        subtitle = plot_subtitle)+
   theme_classic() +
   theme(axis.text = element_text(colour = "black")) +
-  scale_y_continuous(expand = c(0, 0), limits = c(0, 1600))
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 3000))
 
 rap_overall_chart
 
 ggsave(paste0(outputs, "rapid_overall_chart.png"), rap_overall_chart)
+
+# COmparing against same period previous year
+rap_overall_2019_chart <- ggplot(rap_overall_2019_chart_data, aes(x=date, y = count, color = admission_type))+
+  geom_line()+
+  scale_colour_manual(values = pal_overall) +
+  # Axis, plot  and legend titles
+  labs(x = "Date (2020)", y = "Number of admissions", color = "Age group",
+       title = "Number of admissions to hospital in Scotland by type",
+       subtitle = "12 January 2019 - 21 April 2019")+
+  theme_classic() +
+  theme(axis.text = element_text(colour = "black")) +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 3000))
+
+rap_overall_2019_chart
+
+ggsave(paste0(outputs, "rapid_overall_2019_chart.png"), rap_overall_2019_chart)
 
 ###############################################.
 # Day of the week admissions bullet point
@@ -135,15 +159,26 @@ write_csv(rap_overall_bullet, paste0(outputs, "rapid_overall_table.csv"))
 # Chart for deprivation
 rap_depr_chart_data <- rap_pub %>% 
   filter(spec == "All" & type == "depr" & admission_type == "All" &
-           between(date, as.Date("2020-03-22"), as.Date("2020-04-21"))) %>% 
+           # Dates of lockdown
+           between(date, as.Date("2020-03-24"), as.Date("2020-04-21"))) %>% 
   group_by(category) %>% summarise(count = sum(count))
+
+write_csv(rap_depr_chart_data, paste0(outputs, "rap_simd_chart_data.csv"))
+
+rap_depr_2019_chart_data <- rap_pub %>% 
+  filter(spec == "All" & type == "depr" & admission_type == "All" &
+           # Dates of lockdown
+           between(date, as.Date("2019-03-24"), as.Date("2019-04-21"))) %>% 
+  group_by(category) %>% summarise(count = sum(count))
+
+write_csv(rap_depr_2019_chart_data, paste0(outputs, "rap_simd_2019_chart_data.csv"))
 
 rap_depr_chart <- ggplot(rap_depr_chart_data, aes(x=category, y = count))+
   geom_bar(stat = "identity", fill = "#004785")+
   # Axis, plot  and legend titles
   labs(x = "SIMD deprivation quintile", y = "Number of admissions", 
        title = "Number of admissions to hospital in Scotland \nby SIMD deprivation quintile",
-       subtitle = plot_subtitle)+
+       subtitle = "24 March 2020 - 21 April 2020")+
   theme_classic() +
   theme(axis.text = element_text(colour = "black")) +
   scale_y_continuous(expand = c(0, 0), limits = c(0, 10000))
@@ -152,5 +187,19 @@ rap_depr_chart
 
 ggsave(paste0(outputs, "rapid_simd_chart.png"), rap_depr_chart)
 
+# Cheking same period in 2019
+rap_depr_19_chart <- ggplot(rap_depr_2019_chart_data, aes(x=category, y = count))+
+  geom_bar(stat = "identity", fill = "#004785")+
+  # Axis, plot  and legend titles
+  labs(x = "SIMD deprivation quintile", y = "Number of admissions", 
+       title = "Number of admissions to hospital in Scotland \nby SIMD deprivation quintile",
+       subtitle = "24 March 2019 - 21 April 2019")+
+  theme_classic() +
+  theme(axis.text = element_text(colour = "black")) +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 18000))
 
+rap_depr_19_chart
 
+ggsave(paste0(outputs, "rapid_simd_2019_chart.png"), rap_depr_19_chart)
+
+##END
