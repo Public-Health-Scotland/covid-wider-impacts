a96c641b2c46d3fc1763da2c209d3132a984e5c0
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-04-27 18:40:28 +0100

    preparing ooh data

--- app_data_preparation.R
+++ app_data_preparation.R
@@ -7,6 +7,7 @@ library(dplyr)
 library(janitor)
 library(lubridate)
 library(zoo)
+library(readr)
 
 ###############################################.
 ## Functions ----
@@ -59,6 +60,15 @@ agg_rapid <- function(extra_vars = NULL, split, specialty = F) {
 
 
 }
+
+# Speed up aggregations of different data cuts for OOH dataset
+agg_ooh <- function(grouper) {
+  ooh %>%
+    group_by_at(c("week_ending","area_name", "area_type", "date", grouper)) %>%
+    summarise(count = sum(count)) %>% ungroup() %>% 
+    mutate(type = grouper) 
+}
+
 
 ###############################################.
 ## Reading RAPID data ----
@@ -119,33 +129,98 @@ rap_adm <- rbind(rap_adm_all, rap_adm_depr, rap_adm_sex, rap_adm_age) %>%
          admission_type = recode(admission_type, "elective" = "Planned", "emergency" = "Emergency"))
 
 # Preparing data by week for publication
-rap_adm_pub <- rap_adm %>% 
+rap_adm <- rap_adm %>% 
   mutate(week_ending = ceiling_date(date, "week")) %>% #end of week
-  group_by(category, type, admission_type, spec, area_name, week_ending) %>% 
+  group_by(category, type, admission_type, spec, area_name, area_type, week_ending) %>% 
   summarise(count = sum(count)) %>% 
   ungroup()
 
-saveRDS(rap_adm_pub, "data/rapid_data_pub.rds")
+saveRDS(rap_adm, "data/rapid_data_pub.rds")
 
-# Calculating 7 rolling day average, value assigned to last date of period
 rap_adm <- rap_adm %>%
-  group_by(category, type, admission_type, spec, area_name) %>% 
-  mutate(count = round(rollmean(count, 7, fill = NA, align = "right"), 1)) %>% ungroup() %>%
-  # Creating day number to be able to compare pre-covid to covid period
-  mutate(day_year = yday(date))
+  # Creating week number to be able to compare pre-covid to covid period
+  mutate(week_no = isoweek(week_ending))
 
 # Creating average admissions of pre-covid data by day of the year
-rap_adm_old <- rap_adm %>% filter(date<as.Date("2020-01-01")) %>% 
-  group_by(category, type, admission_type, spec, area_name, day_year) %>% 
+rap_adm_old <- rap_adm %>% filter(week_ending<as.Date("2020-01-01")) %>% 
+  group_by(category, type, admission_type, spec, area_name, week_no) %>% 
   summarise(count_average = round(mean(count, na.rm = T), 1)) 
 
 # Joining with 2020 data
-rap_adm_2020 <- left_join(rap_adm %>% filter(date>=as.Date("2020-01-01")), rap_adm_old, 
-                          by = c("category", "type", "admission_type", "spec", "area_name", "day_year"))
+rap_adm_2020 <- left_join(rap_adm %>% filter(between(week_ending, as.Date("2020-01-01"), as.Date("2020-04-20"))), 
+                          rap_adm_old, 
+                          by = c("category", "type", "admission_type", "spec", "area_name", "week_no")) %>% 
+  rename(date = week_ending)
 
 # Temporary for testing purposes: supressing numbers under 5
 rap_adm_2020$count <- ifelse(rap_adm_2020$count<5,0,rap_adm_2020$count)
 
 saveRDS(rap_adm_2020, "shiny_app/data/rapid_data.rds")
+
+###############################################.
+## Preparing OOH data ----
+###############################################.
+ooh <- read_csv("/conf/PHSCOVID19_Analysis/OOH_shiny_app/OOH Weekly Demand_Scot+HBs.csv") %>% 
+  janitor::clean_names() %>% 
+  # Recoding variables to match other datasets
+  mutate(age_group = recode(age_group, "5-14" = "5 - 14", "15-24" = "15 - 44", 
+                           "25-44" = "15 - 44", "45-64" = "45 - 64",
+                           "65-74" = "65 -74", "75-84" = "75 -84",
+                           "85 plus" = "85 and over", "0-4" = "Under 5"),
+         sex = recode(gender, "1" = "Male", "2" = "Female", "0" = NA_character_, "9" = NA_character_),
+         dep = recode(prompt_dataset_deprivation_scot_quintile, 
+                      "1" = "1 - most deprived", "2" = "2",  "3" = "3", 
+                      "4" = "4", "5" = "5 - Least deprived"),
+         week_ending = as.Date(week_ending, "%d/%m/%Y"), #formatting date
+         date = week_ending) %>% 
+  # Selecting consultations for app
+  select(date, week_ending, sex, dep, age = age_group, count = number_of_consultations, 
+         area_name = treatment_nhs_board_name) %>% 
+  #renaming Health boards to match standard used in rest of datasets
+  mutate(area_name = recode(area_name, "NHS AYRSHIRE & ARRAN" = "NHS Ayrshire & Arran", 
+                            "NHS BORDERS" = "NHS Borders", "NHS DUMFRIES & GALLOWAY" = "NHS Dumfries & Galloway", 
+                            "NHS FIFE" = "NHS Fife", "NHS FORTH VALLEY" = "NHS Forth Valley", 
+                            "NHS GRAMPIAN" = "NHS Grampian", "NHS GREATER GLASGOW & CLYDE" = "NHS Greater Glasgow & Clyde", 
+                            "NHS HIGHLAND" = "NHS Highland", "NHS LANARKSHIRE" = "NHS Lanarkshire", 
+                            "NHS LOTHIAN" = "NHS Lothian", "NHS ORKNEY" = "NHS Orkney", 
+                            "NHS SHETLAND" = "NHS Shetland", "NHS TAYSIDE" ="NHS Tayside", 
+                            "NHS WESTERN ISLES" = "NHS Western Isles"),
+         area_type = "Health board") %>% 
+  # Aggregating to make it faster to work with
+  group_by(date, week_ending, sex, dep, age, area_type, area_name) %>% 
+  summarise(count = sum(count, na.rm = T))  %>% ungroup()
+
+# Creating Scotland totals and joining with hb data
+ooh_scot <- ooh %>% group_by(date, week_ending, sex, dep, age) %>% 
+  summarise(count = sum(count, na.rm = T)) %>% 
+  mutate(area_name = "Scotland", area_type = "Scotland") %>% ungroup()
+
+ooh <- rbind(ooh, ooh_scot)
+
+# Creating totals for groups
+ooh_all <- agg_ooh(grouper=NULL) %>% mutate(type = "sex", category = "All")
+ooh_sex <- agg_ooh(grouper="sex") %>% rename(category = sex)
+ooh_dep <- agg_ooh(grouper="dep") %>% rename(category = dep)
+ooh_age <- agg_ooh(grouper="age") %>% rename(category = age)
+ 
+ooh <- rbind(ooh_all, ooh_sex, ooh_dep, ooh_age) %>% 
+  # Creating week number to be able to compare pre-covid to covid period
+  mutate(week_no = isoweek(week_ending))
+
+# Creating average admissions of pre-covid data by day of the year
+ooh_old <- ooh %>% filter(week_ending<as.Date("2020-01-01")) %>% 
+  group_by(category, type, area_name, week_no) %>% 
+  summarise(count_average = round(mean(count, na.rm = T), 1)) 
+
+# Joining with 2020 data
+ooh_2020 <- left_join(ooh %>% filter(between(week_ending, as.Date("2020-01-01"), as.Date("2020-04-20"))), 
+                          ooh_old, 
+                          by = c("category", "type", "area_name", "week_no")) 
+
+# Temporary for testing purposes: supressing numbers under 5
+ooh_2020$count <- ifelse(ooh_2020$count<5,0,ooh_2020$count)
+
+saveRDS(ooh_2020, "shiny_app/data/ooh_data.rds")
+
 
 ##END
