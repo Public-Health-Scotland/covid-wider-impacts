77dfb03d56f2f2d863fe196ae7ebb0119ec239a8
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-04-28 08:59:33 +0100

    spec groupings, making agg function more general

--- app_data_preparation.R
+++ app_data_preparation.R
@@ -8,6 +8,7 @@ library(janitor)
 library(lubridate)
 library(zoo)
 library(readr)
+library(stringr)
 
 ###############################################.
 ## Functions ----
@@ -61,14 +62,13 @@ agg_rapid <- function(extra_vars = NULL, split, specialty = F) {
 
 }
 
-# Speed up aggregations of different data cuts for OOH dataset
-agg_ooh <- function(grouper) {
-  ooh %>%
+# Speed up aggregations of different data cuts 
+agg_cut <- function(dataset, grouper) {
+  dataset %>%
     group_by_at(c("week_ending","area_name", "area_type", "date", grouper)) %>%
     summarise(count = sum(count)) %>% ungroup() %>% 
     mutate(type = grouper) 
 }
-
 
 ###############################################.
 ## Reading RAPID data ----
@@ -87,11 +87,11 @@ rap_adm <- left_join(rap_adm, hb_lookup, by = c("hb" = "hb_cypher")) %>%
   select(-hb) %>% rename(hb = description)
 
 # Bringing spec names
-spec_lookup <- readRDS("/conf/linkage/output/lookups/Unicode/National Reference Files/specialt.rds") %>% 
-  janitor::clean_names() %>% select(description, speccode)
+spec_lookup <- readRDS("/conf/linkage/output/lookups/Unicode/National Reference Files/Specialty_Groupings.rds") %>% 
+  janitor::clean_names() %>% select(grouping, speccode)
 
 rap_adm <- left_join(rap_adm, spec_lookup, by = c("spec" = "speccode")) %>% 
-  select(-spec) %>% rename(spec = description)
+  select(-spec) %>% rename(spec = grouping)
 
 # Aggregating to obtain totals for each split type and then putting all back together
 # Totals for overalls for all pop including totals by specialty too
@@ -106,9 +106,10 @@ rap_adm_sex <- agg_rapid(c("sex"), split = "sex") %>%
 # Totals for overalls for all age groups
 rap_adm_age <- agg_rapid(c("age_group"), split = "age") %>% 
   filter(age_group != "missing") %>% 
-  mutate(category = recode(age_group, "5_thru_14" = "5 - 14", "15_thru_44" = "15 - 44", "45_thru_64" = "45 - 64",
-                           "65_thru_74" = "65 -74", "75_thru_84" = "75 -84",
-                           "85+" = "85 and over", "Under_5" = "Under 5")) %>% 
+  mutate(category = recode_factor(age_group, "Under_5" = "Under 5", "5_thru_14" = "5 - 14", 
+                                  "15_thru_44" = "15 - 44", "45_thru_64" = "45 - 64",
+                                  "65_thru_74" = "65 -74", "75_thru_84" = "75 -84",
+                                  "85+" = "85 and over")) %>% 
   select(-age_group)
 
 # Totals for overalls for deprivation quintiles
@@ -165,10 +166,10 @@ saveRDS(rap_adm_2020, "shiny_app/data/rapid_data.rds")
 ooh <- read_csv("/conf/PHSCOVID19_Analysis/OOH_shiny_app/OOH Weekly Demand_Scot+HBs.csv") %>% 
   janitor::clean_names() %>% 
   # Recoding variables to match other datasets
-  mutate(age_group = recode(age_group, "5-14" = "5 - 14", "15-24" = "15 - 44", 
-                           "25-44" = "15 - 44", "45-64" = "45 - 64",
-                           "65-74" = "65 -74", "75-84" = "75 -84",
-                           "85 plus" = "85 and over", "0-4" = "Under 5"),
+  mutate(age_group = recode_factor(age_group, "0-4" = "Under 5", "5-14" = "5 - 14",  
+                                   "15-24" = "15 - 44", "25-44" = "15 - 44", "45-64" = "45 - 64",
+                                    "65-74" = "65 -74", "75-84" = "75 -84",
+                                    "85 plus" = "85 and over"),
          sex = recode(gender, "1" = "Male", "2" = "Female", "0" = NA_character_, "9" = NA_character_),
          dep = recode(prompt_dataset_deprivation_scot_quintile, 
                       "1" = "1 - most deprived", "2" = "2",  "3" = "3", 
@@ -179,14 +180,7 @@ ooh <- read_csv("/conf/PHSCOVID19_Analysis/OOH_shiny_app/OOH Weekly Demand_Scot+
   select(date, week_ending, sex, dep, age = age_group, count = number_of_consultations, 
          area_name = treatment_nhs_board_name) %>% 
   #renaming Health boards to match standard used in rest of datasets
-  mutate(area_name = recode(area_name, "NHS AYRSHIRE & ARRAN" = "NHS Ayrshire & Arran", 
-                            "NHS BORDERS" = "NHS Borders", "NHS DUMFRIES & GALLOWAY" = "NHS Dumfries & Galloway", 
-                            "NHS FIFE" = "NHS Fife", "NHS FORTH VALLEY" = "NHS Forth Valley", 
-                            "NHS GRAMPIAN" = "NHS Grampian", "NHS GREATER GLASGOW & CLYDE" = "NHS Greater Glasgow & Clyde", 
-                            "NHS HIGHLAND" = "NHS Highland", "NHS LANARKSHIRE" = "NHS Lanarkshire", 
-                            "NHS LOTHIAN" = "NHS Lothian", "NHS ORKNEY" = "NHS Orkney", 
-                            "NHS SHETLAND" = "NHS Shetland", "NHS TAYSIDE" ="NHS Tayside", 
-                            "NHS WESTERN ISLES" = "NHS Western Isles"),
+  mutate(area_name = paste0(toupper(substr(area_name, 1, 3)), substring(str_to_title(area_name), 4)),
          area_type = "Health board") %>% 
   # Aggregating to make it faster to work with
   group_by(date, week_ending, sex, dep, age, area_type, area_name) %>% 
@@ -200,10 +194,10 @@ ooh_scot <- ooh %>% group_by(date, week_ending, sex, dep, age) %>%
 ooh <- rbind(ooh, ooh_scot)
 
 # Creating totals for groups
-ooh_all <- agg_ooh(grouper=NULL) %>% mutate(type = "sex", category = "All")
-ooh_sex <- agg_ooh(grouper="sex") %>% rename(category = sex)
-ooh_dep <- agg_ooh(grouper="dep") %>% rename(category = dep)
-ooh_age <- agg_ooh(grouper="age") %>% rename(category = age)
+ooh_all <- ooh %>% agg_cut(grouper=NULL) %>% mutate(type = "sex", category = "All")
+ooh_sex <- ooh %>% agg_cut(grouper="sex") %>% rename(category = sex)
+ooh_dep <- ooh %>% agg_cut(grouper="dep") %>% rename(category = dep)
+ooh_age <- ooh %>% agg_cut(grouper="age") %>% rename(category = age)
  
 ooh <- rbind(ooh_all, ooh_sex, ooh_dep, ooh_age) %>% 
   # Creating week number to be able to compare pre-covid to covid period
@@ -227,6 +221,5 @@ ooh_2020 <- left_join(ooh %>% filter(between(week_ending, as.Date("2020-01-01"),
 ooh_2020$count <- ifelse(ooh_2020$count<5,0,ooh_2020$count)
 
 saveRDS(ooh_2020, "shiny_app/data/ooh_data.rds")
-
 
 ##END
