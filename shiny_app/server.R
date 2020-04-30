83e63ec29cb1c33437527eb44e536ad08dd340b2
victoe01 <victoria.elliott@nhs.net>
2020-04-30 16:51:14 +0100

    adjust front page text & data sources

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -107,7 +107,7 @@ function(input, output, session) {
       )
     } else if (input$measure_select == "A&E attendances") {
       tagList(#A&E Attendances
-        h3("Weekly attendances to A&E departments (Source: Unscheduled Care Datamart)"),
+        h3("Weekly attendances to A&E departments (Source: AE2 Datamart)"),
         plot_box("2020 compared with the 2018-2019 average", "aye_overall"),
         plot_box(paste0(variation_title, "in 2018-2019 by sex"), "aye_sex"),
         plot_box(paste0(variation_title, "in 2018-2019 by age group"), "aye_age"),
@@ -124,7 +124,7 @@ function(input, output, session) {
       )
     } else if (input$measure_select == "Out of hours consultations") {
       tagList(#Out of hours consultations
-        h3("Weekly consultations to out of hours services (Source: Unscheduled Care Datamart)"),
+        h3("Weekly consultations to out of hours services (Source: GP OOH Datamart)"),
         plot_box("2020 compared with the 2018-2019 average", "ooh_overall"),
         plot_box(paste0(variation_title, "in 2018-2019 by sex"), "ooh_sex"),
         plot_box(paste0(variation_title, "in 2018-2019 by age group"), "ooh_age"),
