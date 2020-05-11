68782ab40235863c9128af3b6accc45896896698
Laura Downie <laura.downie1@nhs.net>
2020-05-11 13:51:36 +0100

    addition of 10May2020 OOH data

--- app_data_preparation.R
+++ app_data_preparation.R
@@ -266,8 +266,32 @@ ooh_new <- ooh_new %>% mutate(scot = "Scotland") %>%
   summarise(count = sum(count, na.rm = T))  %>% ungroup() %>% 
   filter(between(week_ending, as.Date("2020-03-23"), as.Date("2020-05-03")))  #filter complete weeks (Mon-Sun)
 
+#New extract provided for week ending 10 May 2020
+new_ooh_10may2020 <- read_csv("/conf/PHSCOVID19_Analysis/OOH_shiny_app/new_11052020.csv") %>%
+  rename(count=number_of_cases, hscp = hspc) %>%
+  mutate(age = recode_factor(age_group, "0-4" = "Under 5", "5-14" = "5 - 14",  
+                             "15-24" = "15 - 44", "25-44" = "15 - 44", "45-64" = "45 - 64",
+                             "65-74" = "65 - 74", "75-84" = "75 - 84",
+                             "85 plus" = "85 and over"),
+         sex = recode(sex, "1" = "Male", "2" = "Female", "0" = NA_character_, "9" = NA_character_),
+         dep = recode(dep, 
+                      "1" = "1 - most deprived", "2" = "2",  "3" = "3", 
+                      "4" = "4", "5" = "5 - least deprived"),
+         week_ending = as.Date(week_ending, "%d/%m/%Y"), #formatting date
+         scot = "Scotland") %>%
+  subset(week_ending == "2020-05-10")
+
+new_ooh_10may2020 <- new_ooh_10may2020 %>% mutate(scot = "Scotland") %>% 
+  gather(area_type, area_name, c(area_name, hscp, scot)) %>% ungroup() %>% 
+  mutate(area_type = recode(area_type, "area_name" = "Health board", 
+                            "hscp" = "HSC partnership", "scot" = "Scotland")) %>% 
+  # Aggregating to make it faster to work with
+  group_by(week_ending, sex, dep, age, area_name, area_type) %>% 
+  summarise(count = sum(count, na.rm = T))  %>% ungroup() %>% 
+  filter(between(week_ending, as.Date("2020-05-04"), as.Date("2020-05-10")))  #filter complete weeks (Mon-Sun)
+
 #bind old and new ooh data
-ooh <- rbind(ooh_new, ooh)
+ooh <- rbind(new_ooh_10may2020, ooh_new, ooh)
 
 # Creating totals for groups
 ooh_all <- ooh %>% agg_cut(grouper=NULL) %>% mutate(type = "sex", category = "All")
@@ -278,7 +302,7 @@ ooh_age <- ooh %>% agg_cut(grouper="age") %>% rename(category = age)
 ooh <- rbind(ooh_all, ooh_sex, ooh_dep, ooh_age)
 
 # Formatting file for shiny app
-prepare_final_data(dataset = ooh, filename = "ooh", last_week = "2020-05-03")
+prepare_final_data(dataset = ooh, filename = "ooh", last_week = "2020-05-10")
 
 ###############################################.
 ## Preparing A&E data ----
