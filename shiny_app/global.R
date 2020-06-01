2e7e631e3160af10bea9cbd4e826f55b7f4b97ae
lucindalawrie <lucinda.lawrie1@nhs.net>
2020-06-01 11:09:27 +0100

    Update global script (data and palettes)

--- shiny_app/global.R
+++ shiny_app/global.R
@@ -15,6 +15,7 @@ library(shinycssloaders) #for loading icons, see line below
 library(shinyjs) # for enable/disable functions
 library(readr) # for writing/reading csvs
 library(stringr) #for manipulating strings
+library(flextable)
 
 ###############################################.
 ## Functions ----
@@ -47,15 +48,24 @@ ooh <- readRDS("data/ooh_data.rds") # OOH data
 nhs24 <- readRDS("data/nhs24_data.rds") # OOH data
 sas <- readRDS("data/sas_data.rds") # OOH data
 
+## Child Health Data
+six <- readRDS("data/sixinone_data.rds") # 6 in 1 immunisation data at 8 weeks
+sixtable <- readRDS("data/sixinone_datatable.rds") # 6 in 1 immunisation data at 8 weeks datatable summary at 12 weeks
+
 # cath_lab <- readRDS(paste0("shiny_app/data/cath_lab_data.rds"))
-# angio_lab <- readRDS(paste0("shiny_app/data/angio_lab_data.rds"))# Data: GJNH Coronary Angios/PCI 
+# angio_lab <- readRDS(paste0("shiny_app/data/angio_lab_data.rds"))# Data: GJNH Coronary Angios/PCI
 
-spec_list <- sort(c(unique(spec_lookup$'Specialty group'), 
-                  "Medical (incl. Cardiology & Cancer)")) # specialty list
+spec_list <- sort(c(unique(spec_lookup$'Specialty group'),
+                    "Medical (incl. Cardiology & Cancer)")) # specialty list
 
-data_list <- c("Hospital admissions" = "rapid", "A&E attendances" = "aye", 
-               "NHS 24 completed contacts" = "nhs24", 
+data_list <- c("Hospital admissions" = "rapid", "A&E attendances" = "aye",
+               "NHS 24 completed contacts" = "nhs24",
                "Out of hours consultations" = "ooh", "Scottish Ambulance Service" = "sas")
+
+#List of data items available in step 2 of immunisation tab
+data_list_child <- c("6-in-1 first dose" = "sixin_8wks",
+                     "6-in-1 second dose *COMING 10th June 2020*" = "sixin_12wks",
+                     "6-in-1 third dose *COMING 17th June 2020*" = "sixin_16wks")
 
 ###############################################.
 ## Palettes and plot parameters ----
@@ -69,6 +79,10 @@ pal_age <- c('#543005', '#8c510a', '#bf812d',  '#d0d1e6',
 pal_sex <- c('#000000', '#08519c','#bdd7e7')
 pal_overall <- c('#000000', '#009900')
 
+pal_immun <- c("2019" = '#000000', "JAN 2020" = "#abd9e9", "FEB 2020" = "#74add1",
+               "W/B 02-MAR-2020" = "#fee391", "W/B 09-MAR-2020" = "#fec44f",
+               "W/B 16-MAR-2020" = "#fe9929", "W/B 23-MAR-2020" = "#ec7014",
+               "W/B 30-MAR-2020" = "#cc4c02", "W/B 06-APR-2020" = "#8c2d04")
 
 # Style of x and y axis
 xaxis_plots <- list(title = FALSE, tickfont = list(size=14), titlefont = list(size=14),
