9843a1a08488d849b86ac94a1e419e85ac1468e5
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-06-08 12:47:16 +0100

    button for download

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -128,8 +128,8 @@ tabPanel(title = "Child Health", icon = icon("child"), value = "child_health",
                                            label= "Step 2. Select the data you want to explore.", 
                                            choices = data_list_child, status = "primary", 
                                            direction = "vertical", justified = T))),
-           column(4,actionButton("btn_child_modal", "Data source: CHSP-PS, SIRS", icon = icon('question-circle')))#,
-           #column(4,downloadButton("download_chart_data_child", "Download data"))
+           column(4,actionButton("btn_child_modal", "Data source: CHSP-PS, SIRS", icon = icon('question-circle')),
+                  downloadButton("download_visit_data", "Download data"))
            #actionButton("browser", "Browser")
          ), #well panel
          mainPanel(width = 12,
