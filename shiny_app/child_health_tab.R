a85d57fad2a10e14672a6a04b523ea16573f422a
lucindalawrie <lucinda.lawrie1@nhs.net>
2020-06-08 09:42:45 +0100

    change coming soon dates

--- shiny_app/child_health_tab.R
+++ shiny_app/child_health_tab.R
@@ -79,9 +79,9 @@ Coverage rates based on small numbers are prone to fluctuation. Therefore, in Bo
       fluidRow(column(12, renderUI(commentary_first)))
     )
   }  else if (input$measure_select_child == "six_eightwks"){
-    p("6-8 Week Review coming soon")
+    p("6-8 Week Review coming 24th June 2020")
   } else if (input$measure_select_child == "13_15mnth") {
-    p("13-15 Month Review coming soon")
+    p("13-15 Month Review coming 1st July 2020")
   }
   
 }) #close child_health_explorer function
