a38596cea7669b925378554ea47fac9c129ec683
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-06-16 09:52:56 +0100

    for preview

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -12,28 +12,28 @@ function(input, output, session) {
   
   ###############################################.
   # Summary trends tab  
-  source(file.path("summary_tab.R"),  local = TRUE)$value
-  
-  ###############################################.
-  ## Cardiovascular tab
-  source(file.path("cardio_tab.R"),  local = TRUE)$value
-  
-  ###############################################.
-  ## Immunisation tab
-  source(file.path("immunisation_tab.R"),  local = TRUE)$value
-  
-  ###############################################.
-  ## Child Health tab
-  source(file.path("child_health_tab.R"),  local = TRUE)$value
-  
-  ###############################################.
-  ## Perinatal tab
+  # source(file.path("summary_tab.R"),  local = TRUE)$value
+  # 
+  # ###############################################.
+  # ## Cardiovascular tab
+  # source(file.path("cardio_tab.R"),  local = TRUE)$value
+  # 
+  # ###############################################.
+  # ## Immunisation tab
+  # source(file.path("immunisation_tab.R"),  local = TRUE)$value
+  # 
+  # ###############################################.
+  # ## Child Health tab
+  # source(file.path("child_health_tab.R"),  local = TRUE)$value
+  # 
+  # ###############################################.
+  # ## Perinatal tab
   source(file.path("perinatal_tab.R"),  local = TRUE)$value
-  
-  ###############################################.
-  ## Data tab
-  source(file.path("data_tab.R"),  local = TRUE)$value
-  
+  # 
+  # ###############################################.
+  # ## Data tab
+  # source(file.path("data_tab.R"),  local = TRUE)$value
+  # 
   ###############################################.
   ## To move around tabs  
   observeEvent(input$jump_summary, {
