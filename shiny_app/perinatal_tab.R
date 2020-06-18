dea9571a5360f4c3c79297664d09d50960f2efb8
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-06-18 15:37:50 +0100

    review of text

--- shiny_app/perinatal_tab.R
+++ shiny_app/perinatal_tab.R
@@ -8,16 +8,16 @@
 observeEvent(input$btn_perinatal_modal,
              showModal(modalDialog(#RAPID ADMISSIONS MODAL
                title = "What is the data source?",
-               p("The information on perinatal mortality shown in this tool is derived from data provided mostly by ",
+               p("The information on stillbirths and infant mortality shown in this tool is derived 
+                 from deaths and births data provided by ",
                  tags$a(href="https://www.nrscotland.gov.uk/statistics-and-data/statistics/statistics-by-theme/vital-events/deaths/deaths-background-information/stillbirths-and-infant-deaths",
                         "National Records for Scotland.",class="externallink")),
                p(tags$a(href="https://publichealthscotland.scot/","Public Health Scotland (PHS)",class="externallink"),
                  " receives weekly data from NRS on deaths and stillbirths. 
-                 Although PHS usually also receives weekly data on birth registrations from NRS too, 
-                 this has been suspended during the pandemic period. Birth numbers from January 2020 are taken 
-                 from notifications of births in the Child Health Programme System.  ",
-                 (tags$a(href="https://www.isdscotland.org/Health-Topics/Maternity-and-Births/Publications/","publishing",class="externallink")),"
-                 information on birth rates."),
+                  PHS also normally receives weekly data on birth registrations from NRS. 
+                 However birth registrations have been  suspended during the pandemic period. 
+                 So, to provide denominator information, birth numbers from January 2020 are 
+                 taken from notifications of births in the Child Health Programme System."),
                size = "m",
                easyClose = TRUE, fade=FALSE,footer = modalButton("Close (Esc)"))))
 
@@ -25,7 +25,7 @@ observeEvent(input$btn_perinatal_modal,
 observeEvent(input$btn_perinatal_rules,
              showModal(modalDialog(#RAPID ADMISSIONS MODAL
                title = "How do we identify patterns in the data?",
-               p("Control charts follow a series a rules that help identify patterns and trends in the data. 
+               p("Control charts follow a series of rules that help identify important changes in the data. 
                  These are the ones we used in this chart:"),
                tags$ul(tags$li("Outliers: Data points outside the limits marked by the control limits."),
                        tags$li("Shifts: Eight or more consecutive data points above or below the centreline."),
@@ -74,20 +74,23 @@ observeEvent(input$btn_perinatal_rules,
 output$perinatal_explorer <- renderUI({
 
   # text for titles of cut charts
-  perinatal_title <- paste0(case_when(input$measure_select_perinatal == "stillbirths" ~ paste0("Monthly rate of stillbirths per 1,000 births in Scotland",
-                                                                                       input$geoname_perinatal),
-                                      input$measure_select_perinatal == "pnnd" ~ paste0("Monthly rate of post-neonatal deaths per 1,000 births in Scotland",
-                                                                                        input$geoname_perinatal),
-                                      input$measure_select_perinatal == "nnd" ~ paste0("Monthly rate of neonatal deaths per 1,000 births in Scotland",
-                                                                                    input$geoname_perinatal),
-                                      input$measure_select_perinatal == "extperi" ~ paste0("Monthly rate of extended perinatal deaths per 1,000 births in Scotland",
-                                                                                    input$geoname_perinatal),
-                                      input$measure_select_perinatal == "infantdeaths" ~ paste0("Monthly rate of infant deaths per 1,000 births in Scotland",
-                                                                                           input$geoname_perinatal)))
+  perinatal_title <- paste0(case_when(input$measure_select_perinatal == "stillbirths" ~ 
+                                        paste0("Monthly rate of stillbirths per 1,000 total births in Scotland", input$geoname_perinatal),
+                                      input$measure_select_perinatal == "pnnd" ~
+                                        paste0("Monthly rate of post-neonatal deaths per 1,000 live births in Scotland", input$geoname_perinatal),
+                                      input$measure_select_perinatal == "nnd" ~ 
+                                        paste0("Monthly rate of neonatal deaths per 1,000 live births in Scotland", input$geoname_perinatal),
+                                      input$measure_select_perinatal == "extperi" ~ 
+                                        paste0("Monthly rate of extended perinatal deaths per 1,000 total births in Scotland", input$geoname_perinatal),
+                                      input$measure_select_perinatal == "infantdeaths" ~ 
+                                        paste0("Monthly rate of infant deaths per 1,000 live births in Scotland", input$geoname_perinatal)))
   
   # Link used in intro text
-  link_perinatal <- "http://www.healthcareimprovementscotland.org/our_work/reproductive,_maternal__child/reproductive_health/spimmr_2012.aspx"
-  
+  link_perinatal <- "https://www.nrscotland.gov.uk/statistics-and-data/statistics/statistics-by-theme/vital-events/deaths/deaths-background-information/stillbirths-and-infant-deaths"
+  peri_common_intro <- "It is important to monitor the levels of stillbirth and infant mortality 
+                    during the COVID-19 pandemic, as they may be influenced by maternal health 
+  and well-being, by how maternity services are provided, and how people seek and interact with care.
+  NHS Scotland and Scottish Government " 
   # Intro paragraph within perinatal tab
   if (input$measure_select_perinatal == "stillbirths") {
     intro_text <- p("Stillbirths refer to", tags$a(href=link_perinatal, 
@@ -95,37 +98,33 @@ output$perinatal_explorer <- renderUI({
                     "The stillbirth rate in Scotland in 2018 ", 
                     tags$a(href="https://www.isdscotland.org/Health-Topics/Maternity-and-Births/Births/", 
                     "was 3.7 per 1,000 total births.",class="externallink"),
-                    " It is important to monitor the rate of perinatal mortality during the Covid-19 pandemic.",
-                    "NHS Scotland and Scottish Government", 
+                    peri_common_intro,
                     tags$a(href="https://www.nhsinform.scot/illnesses-and-conditions/infections-and-poisoning/coronavirus-covid-19/parents-and-families/coronavirus-covid-19-pregnancy-and-newborn-babies/",
                            "have produced guidelines",class="externallink"), "for attending antenatal and postnatal care appointments during the pandemic.")
   } else if (input$measure_select_perinatal == "pnnd") {
     intro_text <- p("Post-neonatal deaths refer to", tags$a(href=link_perinatal, 
                     "deaths occuring after the first 4 weeks but within the first year",class="externallink"), "of life.",
-                    " It is important to monitor the rate of perinatal mortality during the Covid-19 pandemic.",
-                    "NHS Scotland and Scottish Government", 
+                    peri_common_intro,
                     tags$a(href="https://www.nhsinform.scot/illnesses-and-conditions/infections-and-poisoning/coronavirus-covid-19/parents-and-families/coronavirus-covid-19-pregnancy-and-newborn-babies/",
                            "have produced guidelines",class="externallink"), "for attending antenatal and postnatal care appointments during the pandemic.")
   } else if (input$measure_select_perinatal == "nnd") {
     intro_text <- p("Neonatal deaths refer to", tags$a(href=link_perinatal, 
                   "deaths in the first four weeks",class="externallink"), "of life.",
-                  " It is important to monitor the rate of perinatal mortality during the Covid-19 pandemic.",
-                  "NHS Scotland and Scottish Government", 
+                  peri_common_intro,
                   tags$a(href="https://www.nhsinform.scot/illnesses-and-conditions/infections-and-poisoning/coronavirus-covid-19/parents-and-families/coronavirus-covid-19-pregnancy-and-newborn-babies/",
                          "have produced guidelines",class="externallink"), "for attending antenatal and postnatal care appointments during the pandemic.")
   } else if (input$measure_select_perinatal == "extperi") {
     intro_text <- p("Extended perinatal deaths refer to", tags$a(href=link_perinatal, 
 "the sum of stillbirths and neonatal mortality",class="externallink"), "(deaths within the first 28 days of life).",
-" It is important to monitor the rate of perinatal mortality during the Covid-19 pandemic.",
-"NHS Scotland and Scottish Government", 
+peri_common_intro,
 tags$a(href="https://www.nhsinform.scot/illnesses-and-conditions/infections-and-poisoning/coronavirus-covid-19/parents-and-families/coronavirus-covid-19-pregnancy-and-newborn-babies/",
        "have produced guidelines",class="externallink"), "for attending antenatal and postnatal care appointments during the pandemic.")
     
   } else if (input$measure_select_perinatal == "infantdeaths") {
     intro_text <- p("Infant deaths refer to", tags$a(href=link_perinatal, 
-                    "all deaths in the first year",class="externallink"), "of life.",
-                    " It is important to monitor the rate of perinatal mortality during the Covid-19 pandemic.",
-                    "NHS Scotland and Scottish Government", 
+                    "all deaths in the first year",class="externallink"), "of life; 
+                    this includes neonatal and post-neonatal deaths, but not stillbirths. ",
+                    peri_common_intro,
                     tags$a(href="https://www.nhsinform.scot/illnesses-and-conditions/infections-and-poisoning/coronavirus-covid-19/parents-and-families/coronavirus-covid-19-pregnancy-and-newborn-babies/",
                            "have produced guidelines",class="externallink"), "for attending antenatal and postnatal care appointments during the pandemic.")
   }
@@ -135,18 +134,18 @@ tags$a(href="https://www.nhsinform.scot/illnesses-and-conditions/infections-and-
   tagList(
     fluidRow(column(12, 
                     intro_text,
-                    p("Perinatal mortality is a comparatively rare occurrence in Scotland and for 
-                      this reason we use a particular methodology called ",
+                    p("Stillbirths and infant deaths are (happily) a comparatively rare occurrence in Scotland, 
+                      and for this reason we use a particular methodology called ",
                       tags$a(href= 'https://www.isdscotland.org/health-topics/quality-indicators/statistical-process-control/_docs/Statistical-Process-Control-Tutorial-Guide-180713.pdf',
                              'control charts.'),
                       "These charts help us to identify unusual behaviour in data and indicate
-                      areas that could merit further investigation. Control charts follow a series of rules that help identify patterns 
-                      and trends in the data. Read more about the rules used in this chart by clicking the button below. "),
-                    p("The solid line in this chart shows a time series for of rates for the measure selected, but it also incorporates
-                      other sets of lines: centreline, and control and warning limits.
-                      The centreline is an average (mean) over the time period.
-                      Control and warning limits take in consideration the expected (random) variation of the data and indicate when
-                      values are unexpectedly low or high and require attention. "),
+                      areas that merit further investigation. Control charts follow a series of rules that help identify patterns 
+                      in the data. Read more about the rules used in this chart by clicking the button below: ‘How do we identify patterns in the data?’. "),
+                    p("The dots joined by a solid line in this chart show the rates over time for the measure selected.
+                      The other lines - centreline, and control and warning limits - are there to help show how 
+                      unexpected any observed changes are. The centreline is an average (mean) over the time period. 
+                      Control and warning limits take into consideration the expected (random) variation of the data 
+                      and help us decide when values are unexpectedly low or high and require further investigation."),
                     h4(paste0(perinatal_title)))),
     actionButton("btn_perinatal_rules", "How do we identify patterns in the data?", 
                  icon = icon('question-circle')),
