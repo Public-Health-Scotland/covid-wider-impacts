9f9717fefdb1b1391139b888feccbe81f6acabd7
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-06-18 12:16:24 +0100

    for preview

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -10,62 +10,62 @@ tagList( #needed for shinyjs
 ###############################################.
 ## Introduction ----
 ###############################################.
-tabPanel("Introduction", icon = icon("info-circle"), value = "intro",
-         wellPanel(
-           column(4,
-                  h3("COVID-19 wider impacts on the health care system")),
-           column(8,
-                  tags$br(),
-                  p("The COVID-19 pandemic has wider impacts on individuals’ health, and their use of healthcare services,
-                    than those that occur as the direct result of infection"),
-                  p("Reasons for this may include:"),
-                  tags$ul(
-                    tags$li("Individuals being reluctant to use health services because they do not want to burden
-                            the NHS or are anxious about the risk of infection."),
-                    tags$li("The health service delaying preventative and non-urgent care such as some screening
-                            services and planned surgery."),
-                    tags$li("Other indirect effects of interventions to control COVID-19, such as mental or physical
-                            consequences of distancing measures.")
-                    ),
-                  p("This information tool provides an overview of changes in health and use of healthcare during the COVID-19
-                    pandemic in Scotland, drawing on a range of national data sources."),
-                  p("We are providing information on different topics as quickly as we can, given the different time lags
-                    that apply to different national data sources.  For example, Public Health Scotland receives information
-                    on patients attending Accident & Emergency within days; but there can be a delay of at least six weeks
-                    before we receive detailed information on patients discharged from hospital after having a baby."),
-                  p("Depending on the topic being looked at, information will be shown for patients in different age groups;
-                    for males and females; and for people living in areas with different levels of material deprivation.
-                    Information will also be shown for different locations across Scotland, such as NHS Board areas."),
-                  p("Interactive charts on each of the topics are available in the ",
-                    actionLink("jump_summary","'Summary trends' tab.")),
-                  p("The underlying data used to create the interactive charts can be downloaded using the ",
-                    actionLink( "jump_table", "'Data' tab."),
-                    "Note that some numbers may not sum to the total as disclosure control methods have been applied
-                    to the data in order to protect patient confidentiality."),
-                  p("This tool will be updated weekly. New releases will be published at the same time as the Public Health Scotland ",
-                    tags$a(href="https://beta.isdscotland.org/find-publications-and-data/population-health/covid-19/covid-19-statistical-report/",
-                           "COVID-19 weekly report for Scotland.", class="externallink")),
-                  p("If you have any questions relating to the data presented please contact us at: ",
-                    tags$b(tags$a(href="mailto:phs.covid19analytics@nhs.net", "phs.covid19analytics@nhs.net", class="externallink")), ".")
-                      )
-             ), #wellPanel bracket
-             mainPanel(width = 12
-                      #reserve space for summary text
-             )# mainPanel bracket
-    ), #tabPanel bracket
+# tabPanel("Introduction", icon = icon("info-circle"), value = "intro",
+#          wellPanel(
+#            column(4,
+#                   h3("COVID-19 wider impacts on the health care system")),
+#            column(8,
+#                   tags$br(),
+#                   p("The COVID-19 pandemic has wider impacts on individuals’ health, and their use of healthcare services,
+#                     than those that occur as the direct result of infection"),
+#                   p("Reasons for this may include:"),
+#                   tags$ul(
+#                     tags$li("Individuals being reluctant to use health services because they do not want to burden
+#                             the NHS or are anxious about the risk of infection."),
+#                     tags$li("The health service delaying preventative and non-urgent care such as some screening
+#                             services and planned surgery."),
+#                     tags$li("Other indirect effects of interventions to control COVID-19, such as mental or physical
+#                             consequences of distancing measures.")
+#                     ),
+#                   p("This information tool provides an overview of changes in health and use of healthcare during the COVID-19
+#                     pandemic in Scotland, drawing on a range of national data sources."),
+#                   p("We are providing information on different topics as quickly as we can, given the different time lags
+#                     that apply to different national data sources.  For example, Public Health Scotland receives information
+#                     on patients attending Accident & Emergency within days; but there can be a delay of at least six weeks
+#                     before we receive detailed information on patients discharged from hospital after having a baby."),
+#                   p("Depending on the topic being looked at, information will be shown for patients in different age groups;
+#                     for males and females; and for people living in areas with different levels of material deprivation.
+#                     Information will also be shown for different locations across Scotland, such as NHS Board areas."),
+#                   p("Interactive charts on each of the topics are available in the ",
+#                     actionLink("jump_summary","'Summary trends' tab.")),
+#                   p("The underlying data used to create the interactive charts can be downloaded using the ",
+#                     actionLink( "jump_table", "'Data' tab."),
+#                     "Note that some numbers may not sum to the total as disclosure control methods have been applied
+#                     to the data in order to protect patient confidentiality."),
+#                   p("This tool will be updated weekly. New releases will be published at the same time as the Public Health Scotland ",
+#                     tags$a(href="https://beta.isdscotland.org/find-publications-and-data/population-health/covid-19/covid-19-statistical-report/",
+#                            "COVID-19 weekly report for Scotland.", class="externallink")),
+#                   p("If you have any questions relating to the data presented please contact us at: ",
+#                     tags$b(tags$a(href="mailto:phs.covid19analytics@nhs.net", "phs.covid19analytics@nhs.net", class="externallink")), ".")
+#                       )
+#              ), #wellPanel bracket
+#              mainPanel(width = 12
+#                       #reserve space for summary text
+#              )# mainPanel bracket
+#     ), #tabPanel bracket
 # ###############################################.
 # ## Commentary ----
 # ###############################################.
-#  tabPanel(title = "Commentary", icon = icon("list-ul"), value = "comment",
-#           mainPanel(width = 12,
-#                     uiOutput("summary_comment"),
-#                     uiOutput("deaths_commentary"),
-#                     uiOutput("immun_commentary_section"),
-#                     uiOutput("child_comments"),
-#                     uiOutput("immun_commentary_1705"),
-#                     uiOutput("cardio_commentary")
-#           )#main panel bracket
-#  ), #tab panel
+ tabPanel(title = "Commentary", icon = icon("list-ul"), value = "comment",
+          mainPanel(width = 12,
+                    # uiOutput("summary_comment"),
+                    # uiOutput("deaths_commentary"),
+                    # uiOutput("immun_commentary_section"),
+                    uiOutput("child_comments")#,
+                    # uiOutput("immun_commentary_1705"),
+                    # uiOutput("cardio_commentary")
+          )#main panel bracket
+ ), #tab panel
 # ###############################################.
 # ## Summary trends ----
 # ###############################################.
