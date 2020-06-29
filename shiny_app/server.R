b3c5d8f18c864eee738f709fa4aecfb0e64fc9ca
Laura Downie <laura.downie1@nhs.net>
2020-06-29 12:25:13 +0100

    perinatal tab addition

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -3,8 +3,8 @@
 function(input, output, session) {
   
   # For debugging
-  # observeEvent(input$browser, browser())
-  
+   observeEvent(input$browser, browser())
+
   ###############################################.
   ## Functions 
   # Sourcing file with functions code
@@ -12,19 +12,19 @@ function(input, output, session) {
   
   ###############################################.
   # Summary trends tab  
-  # source(file.path("summary_tab.R"),  local = TRUE)$value
+  source(file.path("summary_tab.R"),  local = TRUE)$value
   # 
   # ###############################################.
   # ## Cardiovascular tab
-  # source(file.path("cardio_tab.R"),  local = TRUE)$value
+  source(file.path("cardio_tab.R"),  local = TRUE)$value
   # 
   # ###############################################.
   # ## Immunisation tab
-  # source(file.path("immunisation_tab.R"),  local = TRUE)$value
+  source(file.path("immunisation_tab.R"),  local = TRUE)$value
   # 
   # ###############################################.
   # ## Child Health tab
-  # source(file.path("child_health_tab.R"),  local = TRUE)$value
+  source(file.path("child_health_tab.R"),  local = TRUE)$value
   # 
   # ###############################################.
   # ## Perinatal tab
@@ -32,7 +32,7 @@ function(input, output, session) {
   # 
   # ###############################################.
   # ## Data tab
-  # source(file.path("data_tab.R"),  local = TRUE)$value
+  source(file.path("data_tab.R"),  local = TRUE)$value
   # 
   ###############################################.
   ## To move around tabs  
@@ -40,10 +40,10 @@ function(input, output, session) {
     updateTabsetPanel(session, "intabset", selected = "summary")
   })
   
-  # observeEvent(input$jump_cardio, {
-  #   updateTabsetPanel(session, "intabset", selected = "cardio")
-  # })
-  
+  observeEvent(input$jump_cardio, {
+    updateTabsetPanel(session, "intabset", selected = "cardio")
+  })
+
   observeEvent(input$jump_table, {
     updateTabsetPanel(session, "intabset", selected = "table")
   })
