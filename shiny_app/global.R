8fb47aa7327591ad32b6b38df3968714f7f1e4ca
victoe01 <victoria.elliott@nhs.net>
2020-08-05 17:23:59 +0100

    update the extract date and make it so it only needs to changed once in the global script. Also borrow addition month colour palette from immunisation update

--- shiny_app/global.R
+++ shiny_app/global.R
@@ -68,7 +68,7 @@ cardio_drugs <- readRDS("data/cardio_drugs_data.rds") # Cardio drugs data
 cath_lab <- readRDS("data/cath_lab_data.rds") # Cath lab data
 
 ## Child Health Data
-child_extract_date <- "22nd June 2020"
+child_extract_date <- "27th July 2020"
 first <- readRDS("data/first_visit_data.rds") # first health visit at 2 weeks
 firsttable <- readRDS("data/first_visit_datatable.rds")
 sixtoeight <- readRDS("data/six_to_eight_data.rds")
@@ -168,7 +168,7 @@ pal_immun <- c("2019" = '#000000',
                "W/B 27-APR-2020" = "#8c2d04", "W/B 04-MAY-2020" = "#662506")
 
 pal_child <- c("2019" = '#000000', "JAN 2020" = "#abd9e9", "FEB 2020" = "#74add1",
-               "MAR 2020" = "#0570b0", "APR 2020" = "#0570b0", 
+               "MAR 2020" = "#0570b0", "APR 2020" = "#045a8d", 
                "W/B 27-APR-2020" = "#fec44f",
                "W/B 04-MAY-2020" = "#fe9929", "W/B 11-MAY-2020" = "#ec7014",
                "W/B 18-MAY-2020" = "#cc4c02", "W/B 25-MAY-2020" = "#8c2d04",
