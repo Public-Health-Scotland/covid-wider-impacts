30787c0f29c5d2e40cf228d5e24a11ce6893c36a
Laura Downie <laura.downie1@nhs.net>
2020-08-17 10:41:40 +0100

    Updates - RAPID ONLY

--- app_data_preparation.R
+++ app_data_preparation.R
@@ -9,7 +9,7 @@ source("functions_packages_data_prep.R")
 ## RAPID data ----
 ###############################################.
 # Prepared by Unscheduled care team
-rap_adm <- readRDS(paste0(data_folder, "rapid/Admissions_by_category_10-Aug.rds")) %>% 
+rap_adm <- readRDS(paste0(data_folder, "rapid/Admissions_by_category_17-Aug.rds")) %>% 
   janitor::clean_names() %>% 
   # taking out aggregated values, not clear right now
   filter(!(substr(hosp,3,5) == "All" | (substr(hscp_name,3,5) == "All")) &
@@ -97,7 +97,7 @@ rap_adm <- rbind(rap_adm, spec_med, paed_com) %>%
   # Excluding specialties groups with very few cases and of not much interest
   filter(!(spec %in% c("Dental", "Other"))) 
 
-prepare_final_data(rap_adm, "rapid", last_week = "2020-08-02", 
+prepare_final_data(rap_adm, "rapid", last_week = "2020-08-09", 
                    extra_vars = c("admission_type", "spec"))
 
 ###############################################.
