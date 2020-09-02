0d8343f6bbf8161dbb05842bbfbb9965cdee16ce
colinh01 <colin.houston@nhs.net>
2020-09-02 08:47:57 +0100

    Data tab update

--- shiny_app/data_tab.R
+++ shiny_app/data_tab.R
@@ -18,6 +18,7 @@ data_table <- reactive({
          "cath_lab" = cath_lab %>% rename(average_2018_2019 = count_average),
          "ooh_cardiac" = ooh_cardiac %>% rename(average_2018_2019 = count_average),
          "nhs24_cardiac" = nhs24_cardiac %>% rename(average_2018_2019 = count_average),
+         "sas_cardiac" = sas_cardiac %>% rename(average_2018_2019 = count_average),        
          "sixin_8wks" = sixtable,
          "sixin_8wks_second" = sixtable_dose2,
          "sixin_8wks_third" = sixtable_dose3,
@@ -34,7 +35,7 @@ data_table <- reactive({
     # appear for factors
     mutate_if(is.character, as.factor) 
   
-  if (input$data_select %in% c("rapid", "aye", "nhs24", "ooh", "sas", "deaths", "nhs24_cardiac", "ooh_cardiac")) {
+  if (input$data_select %in% c("rapid", "aye", "nhs24", "ooh", "sas", "deaths", "nhs24_cardiac", "ooh_cardiac", "sas_cardiac")) {
     table_data <- table_data %>% 
     # Formatting to a "nicer" style
     select(-type) %>% 
