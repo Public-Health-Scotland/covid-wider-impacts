e2686d4f38366d3174702cf7289ce8a6d77f733f
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-09-15 12:02:54 +0100

    modal rules, traces rules, y axis range

--- shiny_app/child_dev_tab.R
+++ shiny_app/child_dev_tab.R
@@ -6,12 +6,30 @@
 
 # Pop-up modal explaining source of data
 observeEvent(input$btn_childdev_modal,
-             showModal(modalDialog(#RAPID ADMISSIONS MODAL
+             showModal(modalDialog(
                title = "What is the data source?",
                p("Data source: xxxx."),
                p("Placeholder"),
                p("Data for NHS Greater Glasgow and Clyde is only available from May 2019 onwards."),
                p("The average is calculated as the median value of the period specified."),
+               size = "m",
+               easyClose = TRUE, fade=FALSE,footer = modalButton("Close (Esc)"))))
+
+# Modal to explain SPC charts rules
+observeEvent(input$btn_childdev_rules,
+             showModal(modalDialog(
+               title = "How do we identify patterns in the data?",
+               p("COntrols charts follow a series of rules that help identify important changes in the data. 
+                 These are the ones we used in this chart:"),
+               tags$ul(tags$li("Shifts: Six or more consecutive data points above or below the centreline."),
+                       tags$li("Trends: Five or more consecutive data points which are increasing or decreasing."),
+                       tags$li("Outer One – Third: Two out of three consecutive data points which sit between the control and warning limits."),
+                       tags$li("Inner One -Third: 15 or more consecutive data points that lie close to the centreline.")),
+               p("Different control charts are used depending on the type of data involved.
+                 For the child development ones we have used run charts."),
+               p("Further information on these methods of presenting data can be found at the ",                      
+                 tags$a(href= 'https://www.isdscotland.org/health-topics/quality-indicators/statistical-process-control/_docs/Statistical-Process-Control-Tutorial-Guide-180713.pdf',
+                        'PHS guide to statistical process control charts.')),
                size = "m",
                easyClose = TRUE, fade=FALSE,footer = modalButton("Close (Esc)"))))
 
@@ -61,6 +79,8 @@ output$childdev_explorer <- renderUI({
     fluidRow(column(12, 
                     h4(paste0("Percentage of children with 1 or more developmental concerns at the ",  
                               review_title, " review")))),
+    actionButton("btn_childdev_rules", "How do we identify patterns in the data?", 
+                 icon = icon('question-circle')),
     fluidRow(withSpinner(plotlyOutput("childdev_no_concerns")))
     )#tagLIst bracket
   
@@ -118,6 +138,7 @@ output$childdev_no_concerns <- renderPlotly({
   
   #Modifying standard layout
   yaxis_plots[["title"]] <- "Percentage (%)"
+  yaxis_plots[["range"]] <- c(0, 50)  # forcing range from 0 to 100%
   xaxis_plots[["range"]] <- c(min(trend_data$month_review), max(trend_data$month_review))
   
   tooltip_trend <- c(paste0("Month:", format(trend_data$month_review, "%b %y"),
@@ -130,7 +151,13 @@ output$childdev_no_concerns <- renderPlotly({
   run_plot <- plot_ly(data=trend_data, x=~month_review) %>%
     add_lines( y = ~pc_1_plus,  
               line = list(color = "black"), text=tooltip_trend, hoverinfo="text",
-              marker = list(color = "black"), name = "% children with developmental concerns")
+              marker = list(color = "black"), name = "% children with developmental concerns") %>% 
+    # adding trends
+    add_markers(data = trend_data %>% filter(trend == T), y = ~ rate,
+                marker = list(color = "green", size = 10, symbol = "square"), name = "Trends") %>%  
+    # adding shifts
+    add_markers(data = trend_data %>% filter(shift == T), y = ~ rate,
+                marker = list(color = "blue", size = 10, symbol = "circle"), name = "Shifts") %>% 
   
   # Dotted line for projected tails of centreline. It changes depending on area.
   if (input$geoname_childdev %in% c("Scotland", "NHS Greater Glasgow & Clyde")) {
