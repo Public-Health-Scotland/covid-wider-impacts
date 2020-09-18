b1f6a52cbca2cd8055e5da96dee0000b1499bd37
murdoturner <67368485+murdoturner@users.noreply.github.com>
2020-09-18 16:16:09 +0100

    Update cancer_tab.R

--- shiny_app/cancer_tab.R
+++ shiny_app/cancer_tab.R
@@ -7,56 +7,29 @@
 observeEvent(input$btn_cancer_modal, 
              showModal(modalDialog(
                title = "What is the data source?",
-               p("Cancer is not a statutorily notifiable disease, so cases are not reported prospectively. 
-                  Instead reliant on a complex process involving record linkage and data processing of multiple 
-                  records from multiple sources of potentially new cases of cancer, with the aim of maximising 
-                  case ascertainment and data accuracy:  ",
+               p("",
                  tags$a(href="https://www.isdscotland.org/Health-Topics/Cancer/Scottish-Cancer-Registry/How-data-are-collected/",class="externallink")),
-               p("In an effort to more rapidly assess the impact of COVID-19 on new diagnoses of cancer, opted 
-                  to analyse pathology records, comparing trends in weekly numbers of cancer diagnoses 
-                  (based on dates of pathology specimen receipt) for 2020 compared to 2019."),
-               em("Advantages of using pathology records:"),
-                p("- Pathology records are one of the main sources of new incident cases for the cancer registry."),
-                 p("- For many anatomical sites of cancer, a pathology report is generated for the overwhelming majority of new cases "),
-                  p("- •	In most cases, a pathology record is received by the cancer registry within weeks of the definitive diagnosis being made."),
-              em("Limitations of using pathology records:"),
-                p("- The SNOMED topography code assigned to a pathology record does not necessarily relate to the primary site of origin of a tumour,
-                  especially if there has been localised spread to adjacent organs, or further afield (regional lymph nodes or distant metastases), or in 
-                  the case of multi-organ cancers, eg, some lympho-haematopoietic neoplasms."),
-                 p("- While the majority of pathology records relate to new incident cancers, some records relate to disease recurrences and/or metastatic disease. "),
-                  p("- As with any system of data collection, there can be errors in the SNOMED code allocated, but these are relatively rare."),
-                  p("- In some cases, multiple pathology records are generated for the same incident tumour, eg, breast needle biopsy, wide local excision specimen, 
-                    axillary lymph node biopsy, etc. This can be mitigated to some extent by restricting to a single record per person (although some individuals will 
-                    genuinely have multiple synchronous independent primary tumours), or to a single record per major tumour site (although some individuals will genuinely 
-                    have multiple synchronous independent primary tumours at the same anatomical site and, as noted above, some records may relate to metastatic rather 
-                    than primary disease, eg, secondary tumours of the liver)."),
-                  p("- Full reporting of some pathology records may be delayed for several weeks, eg, if a second opinion and/or information on tumour biomarkers is 
-                    being sought."),
-
+               p(""),
+               p(""),
+               p(""),
+               
                p("Data are reported by NHS Board of treatment "),
-               p(paste0("Figures presented based on data extracted on ",cancer_extract_date)),
+               p(paste0("Figures presented based on data extracted on ",cancer_extract_date)), # need to define cancer_extract_date reactive value
                size = "m",
                easyClose = TRUE, fade=FALSE,footer = modalButton("Close (Esc)"))))
 
 ###############################################.
 ## Reactive datasets ----
 ###############################################.
-cath_lab_chosen <- reactive({
-  cath_lab %>% filter(lab == input$area_cardio_select) %>% droplevels()
+
+cancer_data_hb <- reactive({
+  cancer_data %>% filter(health_board_name == input$geoname_cancer) 
 })
 
-cath_lab_over <- reactive({
-  cath_lab_chosen() %>% filter(groups == "Angiography") %>% droplevels()
+cancer_data_type <- reactive({
+  cancer_data_hb() %>% filter(site_descript == input$cancer_type_select) 
 })
 
-cath_lab_adm <- reactive({
-  cath_lab_chosen() %>% filter(type == "adm" & groups == "Percutaneous coronary intervention") %>% droplevels()
-})
-
-cath_lab_type <- reactive({
-  cath_lab_chosen() %>% filter(category == "All") %>% 
-    select(-category) %>% rename(category = groups) %>% droplevels()
-})
 
 
 ###############################################.
@@ -65,7 +38,7 @@ cath_lab_type <- reactive({
 
 # Cancer reactive drop-down control showing list of area names depending on areatype selected
 output$geoname_ui_cancer <- renderUI({
-    #Lists areas available in   
+  #Lists areas available in   
   areas_summary_cancer <- sort(geo_lookup$areaname[geo_lookup$areatype == input$geotype_cancer])
   selectizeInput("geoname_cancer", label = NULL, choices = areas_summary_cancer, selected = "")
 })
@@ -88,118 +61,55 @@ output$cancer_explorer <- renderUI({
                        input$cancer_type_select == "uppergi" ~ "uppergi",
                        input$cancer_type_select == "urological" ~ "urological")
   
- 
-  variation_title <- paste0("Percentage change in number of pathology referrals for ", dataset, 
-                              " cancer compared with the corresponding time in 2018-2019")
-
-
-  total_title <- paste0("Weekly change in pathology incidence for ", dataset, " cancer ")
+  
+  cancer_chart_title <- paste0("Percentage change in number of pathology referrals for ", dataset, 
+                               " cancer compared with the corresponding time in 2018-2019 by")
   
   
-  
-  
-  
-  
-  
-  # To make sure that both titles take the same space and are lined up doing
-  # a bit of a hacky shortcut:
-  diff_chars <- nchar(variation_title) - nchar(total_title) +10
-  extra_chars <- paste0(c(rep("_", diff_chars), "."), collapse = '')
-
   # Function to create the standard layout for all the different charts/sections
   cut_charts <- function(title, source, data_name) {
     tagList(
       h3(title),
       actionButton("btn_dataset_modal", paste0("Data source: ", source), icon = icon('question-circle')),
       
-        tagList(
-          plot_box(paste0("2020 compared with the 2018-2019 average"), paste0(data_name, "_overall")),
-          
-          plot_cut_box(paste0(variation_title, "sex"), paste0(data_name, "_sex_var")),
-                   
-          plot_cut_box(paste0(variation_title, "age group"), paste0(data_name, "_age_var")),
-                  
-          fluidRow(column(6, h4(paste0(variation_title, "SIMD quintile"))),
-               column(6, h4(paste0(total_title, "SIMD quintile")))),
-          fluidRow(actionButton("btn_modal_simd", "What is SIMD and deprivation?", 
-                            icon = icon('question-circle'))),
-          
-          plot_cut_box(paste0(variation_title, "deprivation"), paste0(data_name, "_depr_var")))))
-               
-    )
-    
+      tagList(
+        plot_box(paste0("2020 compared with the 2018-2019 average"), "cancer_type"),
+        
+        plot_cut_box(paste0(cancer_chart_title, "sex"), "cancer_type"),
+        
+        plot_cut_box(paste0(cancer_chart_title, "age group"), "cancer_type"),
+        
+        fluidRow(column(6, h4(paste0(cancer_chart_title, "SIMD quintile")))),
+        
+        fluidRow(actionButton("btn_modal_simd", "What is SIMD and deprivation?", 
+                              icon = icon('question-circle'))),
+        
+        plot_cut_box(paste0(cancer_chart_title, "SIMD quintile"), "cancer_type")))
   }
   
   # Charts and rest of UI
-  if (input$cancer_type_select == "all_types") {
-    tagList(
-      cut_charts(title= "Weekly pathology referrals", source = "",
-                 data_name = "all_types"),
-    )
-  } else if (input$cancer_type_select == "breast") { 
-    cut_charts(title= "Weekly pathology referrals", source = "", 
-               data_name = "breast")
-    
-  } else if (input$cancer_type_select == "cervical") { 
-    cut_charts(title= "Weekly pathology referrals", source = "", 
-               data_name = "cervical")
-    
-  } else if (input$cancer_type_select == "colorectal") {
-    cut_charts(title= "Weekly pathology referrals", 
-               source = "", 
-               data_name ="colorectal")
-    
-  } else if (input$cancer_type_select == "headandneck") { 
-    cut_charts(title= "Weekly pathology referrals", 
-               source = "", 
-               data_name ="ooh")
-    
-  } else if (input$cancer_type_select == "lung") { 
-    cut_charts(title= "Weekly pathology referrals", 
-               source = "", 
-               data_name ="lung")
-    
-  } else if (input$cancer_type == "lymphoma") { 
-    cut_charts(title= "Weekly pathology referrals", 
-               source = "", 
-               data_name ="lymphoma")
-    
-  } else if (input$cancer_type == "melanoma") { 
-    cut_charts(title= "Weekly pathology referrals", 
-               source = "", 
-               data_name ="melanoma")
-    
-  } else if (input$cancer_type == "ovarian") { 
-    cut_charts(title= "Weekly pathology referrals", 
-               source = "", 
-               data_name ="ovarian")
-    
-  } else if (input$cancer_type == "uppergi") { 
-    cut_charts(title= "Weekly pathology referrals", 
-               source = "", 
-               data_name ="uppergi")
-    
-  } else if (input$cancer_type == "urological") { 
-    cut_charts(title= "Weekly pathology referrals", 
-               source = "NRS Death Registrations", 
-               data_name ="urological")
-  } 
-}) 
+  
+  cut_charts(title= "Weekly pathology referrals", source = "",
+             data_name = "cancer_data_type")
+  
+})
 
 ###############################################.
 ## Charts ----
 ###############################################.
 # Creating plots for each cut and dataset
 
-output$cancer_overall <- renderPlotly({plot_overall_chart(aye, data_name = "all_types")})
-output$cancer_sex_var <- renderPlotly({plot_trend_chart(aye, pal_sex, "sex", data_name = "aye")})
-output$cancert_age_var <- renderPlotly({plot_trend_chart(aye, pal_age, "age", data_name = "aye")})
-output$cancer_depr_var <- renderPlotly({plot_trend_chart(aye, pal_depr, "dep", data_name = "aye")})
+output$cancer_overall <- renderPlotly({plot_overall_chart(cancer_data_type(), data_name = "cancer_data_type")})
+output$cancer_sex_var <- renderPlotly({plot_trend_chart(cancer_data_type(), pal_sex, "sex", data_name = "cancer_data_type")})
+output$cancert_age_var <- renderPlotly({plot_trend_chart(cancer_data_type(), pal_age, "age", data_name = "cancer_data_type")})
+output$cancer_depr_var <- renderPlotly({plot_trend_chart(cancer_data_type(), pal_depr, "dep", data_name = "cancer_data_type")})
 
 
 ###############################################.
 ## Data downloads ----
 ###############################################.
+
+# 
 
 ###############################################.
 ## Commentary ----
@@ -209,35 +119,31 @@ output$cancer_commentary <- renderUI({
     bsButton("jump_to_cancer",label = "Go to data"), #this button can only be used once
     h2("Cancer - xxth xxx 2020"), 
     
-          h3("Prescribing"),
-          p(""),
-          tags$ul(
-            tags$li(""),
-            tags$li("",
-                    tags$ul(
-                      tags$li(""),
-                      tags$li(""),
-                      tags$li("By the end of May, the numbers of prescriptions were returning
-                              to normal levels.")
-                    )
-            )
-          ),
-          h3("Cardiovascular A&E attendances"),
-          p(""),
-          tags$ul(
-            tags$li(""),
-            tags$li("This drop in cardiovascular attendances was consistent across both males and
-                    females, in younger and older patients and across deprivation quintiles.")),
-          h3("Cardiac procedures"),
-          p(""),
-          tags$ul(
-            tags$li("coronary angiography "),
-            tags$li("cardiac devices"),
-            tags$li("percutaneous coronary interventions")),
-          p("The major observations are as follows:"),
-          tags$ul(
-            tags$li(""),
-            tags$li(""),
-            tags$li(""))
-           )
+    h3("Commentary"),
+    p(""),
+    tags$ul(
+      tags$li("Cancer is not a statutorily notifiable disease, so cases are not reported prospectively. 
+              Instead reliant on a complex process involving record linkage and data processing of multiple 
+              records from multiple sources of potentially new cases of cancer, with the aim of maximising 
+              case ascertainment and data accuracy: "),
+      tags$li("In an effort to more rapidly assess the impact of COVID-19 on new diagnoses of cancer, opted 
+              to analyse pathology records, comparing trends in weekly numbers of cancer diagnoses 
+              (based on dates of pathology specimen receipt) for 2020 compared to 2019.",
+              tags$ul(
+                tags$li("Advantages of using pathology records:"),
+                tags$li("- Pathology records are one of the main sources of new incident cases for the cancer registry."),
+                tags$li("- For many anatomical sites of cancer, a pathology report is generated for the overwhelming majority of new cases"),
+                tags$li("-	In most cases, a pathology record is received by the cancer registry within weeks of the definitive diagnosis being made."),
+                tags$li("Limitations of using pathology records:"),
+                tags$li("- While the majority of pathology records relate to new incident cancers, some records relate to disease recurrences and/or metastatic disease."),
+                tags$li("- As with any system of data collection, there can be errors in the SNOMED code allocated, but these are relatively rare."),
+                tags$li("- In some cases, multiple pathology records are generated for the same incident tumour, eg, breast needle biopsy, wide local excision specimen, 
+                        axillary lymph node biopsy, etc. This can be mitigated to some extent by restricting to a single record per person (although some individuals will 
+                        genuinely have multiple synchronous independent primary tumours), or to a single record per major tumour site (although some individuals will genuinely 
+                        have multiple synchronous independent primary tumours at the same anatomical site and, as noted above, some records may relate to metastatic rather 
+                        than primary disease, eg, secondary tumours of the liver).")
+                )
+              )
+              )
+              )
 })
