cbf34c45f72bbc8e456199d9ef73ea7cd4b9cc0e
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-11-13 14:55:42 +0000

    saving cancer data

--- app_data_preparation.R
+++ app_data_preparation.R
@@ -1011,20 +1011,13 @@ perinatal %<>%
 
 saveRDS(perinatal, paste0("shiny_app/data/","perinatal_data.rds"))
 
-##########################################################
-## Cancer Data
-##########################################################
+##########################################################.
+## Cancer Data ----
+##########################################################.
 
-cancer_cum <- cancer_data1 %>%
-  filter(category == "sex") %>%
-  rename(sex = type) %>%
-  select(area, site, sex, week_ending, count19, count20, difference) %>%
-  group_by(area, site, sex) %>%
-  mutate(cum_count19 = cumsum(count19),
-         cum_count20 = cumsum(count20))
+cancer <- readRDS(paste0(data_folder,"cancer/cancer_data_2.rds"))
 
-
-saveRDS(cancer_cum, "shiny_app/data/cancer_data_2.rds")
+saveRDS(cancer, "shiny_app/data/cancer_data_2.rds")
 
 
 
