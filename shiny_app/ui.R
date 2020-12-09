ce5c9556abcf3b0fa2e9ea55ba6d8374aff6bdda
Lucinda Lawrie <lucinda.lawrie@phs.scot>
2020-12-09 09:56:49 +0000

    ready for PRA

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -1,5 +1,5 @@
 #UI
-#secure_app( #uncomment if needing password protection
+secure_app( #uncomment if needing password protection
 
 tagList( #needed for shinyjs
   useShinyjs(),  # Include shinyjs
@@ -500,5 +500,5 @@ tabPanel(title = "Gestation at delivery", icon = icon("calendar-alt"), value = "
 #       ) # tabpanel bracket
 #     ) #page bracket
 #   ) #taglist bracket
-# ) #secure app
+ ) #secure app
 #END
