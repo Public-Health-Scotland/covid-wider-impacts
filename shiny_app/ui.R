89ec828d6a6cf722ad38b0fe0740ce90a177122a
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-12-18 16:28:52 +0000

    secure app out

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -1,5 +1,5 @@
 #UI
- secure_app( #uncomment if needing password protection
+ # secure_app( #uncomment if needing password protection
 
 tagList( #needed for shinyjs
   useShinyjs(),  # Include shinyjs
@@ -390,5 +390,5 @@ tabPanel(title = "Termination of pregnancy", icon = icon("bars"), value = "termi
       ) # tabpanel bracket
     )# page bracket
  )# taglist bracket
- )#secure app
+ # )#secure app
 #END
