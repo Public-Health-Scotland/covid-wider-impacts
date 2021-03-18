cbdeb339451912ada8e64e0b28e5e987f64b642f
lucindalawrie <lucinda.lawrie1@nhs.net>
2021-03-18 09:53:35 +0000

    update extract date

--- shiny_app/global.R
+++ shiny_app/global.R
@@ -128,7 +128,7 @@ booking <- readRDS("data/ante_booking.rds")
 booking_download <- readRDS("data/ante_booking_download.rds")
 
 #terminations
-top_extract_date <- "9th February 2021"
+top_extract_date <- "11th March 2021"
 top <- readRDS("data/top.rds")
 top_download <- readRDS("data/top_download.rds")
 
