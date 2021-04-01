a82bdb70b94426e922062f44024f609e9a9f3b19
Jaime Villacampa <jaime.villacampa@phs.scot>
2021-04-01 16:49:18 +0100

    board names, board info from smr, fixes for unknown values

--- data_prep/deaths_data_preparation.R
+++ data_prep/deaths_data_preparation.R
@@ -332,13 +332,13 @@ channel <- suppressWarnings(dbConnect(odbc(),  dsn="SMRA",
 # Bringing  LA and datazone info.
 postcode_lookup <- readRDS('/conf/linkage/output/lookups/Unicode/Geography/Scottish Postcode Directory/Scottish_Postcode_Directory_2020_2.rds') %>% 
   setNames(tolower(names(.))) %>%   #variables to lower case
-  select(pc7, datazone2011, hb2019, hscp2019)
+  select(pc7, datazone2011, hscp2019)
 
 # SIMD quintile to datazone lookup
 dep_lookup <- readRDS("/PHI_conf/ScotPHO/Profiles/Data/Lookups/Geography/deprivation_geography.rds") %>%
   rename(datazone2011 = datazone) %>%
   select(datazone2011, year, sc_quin) %>% 
-  filter(year>2015)
+  filter(year>2014)
 
 dep_lookup20 <- dep_lookup %>%  filter(year == 2019) %>% mutate(year = 2020)
 dep_lookup21 <- dep_lookup %>%  filter(year == 2019) %>% mutate(year = 2021)
@@ -351,11 +351,13 @@ geo_lookup <- left_join(dep_lookup, postcode_lookup)
 ## Extract deaths data from SMRA ----
 ###############################################.
 data_deaths <- as_tibble(dbGetQuery(channel, statement=
-"SELECT date_of_registration, age, sex, UNDERLYING_CAUSE_OF_DEATH diag, POSTCODE pc7
+"SELECT date_of_registration, age, sex, UNDERLYING_CAUSE_OF_DEATH diag, 
+        HBRES_CURRENTDATE hb, POSTCODE pc7
     FROM ANALYSIS.GRO_DEATHS_C
     WHERE date_of_registration >= '29 December 2014'
   UNION ALL
-    SELECT date_of_registration, age, sex, UNDERLYING_CAUSE_OF_DEATH diag, POSTCODE pc7
+    SELECT date_of_registration, age, sex, UNDERLYING_CAUSE_OF_DEATH diag, 
+        HB9 hb, POSTCODE pc7
     FROM ANALYSIS.GRO_DEATHS_WEEKLY_C")) %>%
   setNames(tolower(names(.))) %>% 
 # Formatting variables
@@ -377,7 +379,7 @@ data_deaths <- left_join(data_deaths, geo_lookup) %>% select(-datazone2011)
 #Pivoting so one row per area
 data_deaths %<>% 
   mutate(scot = "Scotland") %>% 
-  pivot_longer(cols = c(hb2019, hscp2019, scot)) %>% 
+  pivot_longer(cols = c(hb, hscp2019, scot)) %>% 
   #filtering out NA duplicates (which are counted in Scotland totals, but not elsewhere)
   filter(!is.na(value)) %>% 
   # More formatting
@@ -386,8 +388,10 @@ data_deaths %<>%
          dep = recode(sc_quin, 
                       "1" = "1 - most deprived", "2" = "2",  "3" = "3", 
                       "4" = "4", "5" = "5 - least deprived"),
-         area_type = recode(name, "hb2019" = "Health board", 
+         area_type = recode(name, "hb" = "Health board", 
                             "hscp2019" = "HSC partnership", "scot" = "Scotland")) %>% 
+  mutate(area_name = case_when(area_type=="Health board" ~ (paste0("NHS ",gsub(" and ", " & ", area_name))), 
+                               TRUE~area_name)) %>% 
   # Aggregating to make it faster to work with
   group_by(week_ending, sex, dep, age, area_name, area_type) %>% 
   summarise(count = n())  %>% ungroup()
@@ -407,14 +411,15 @@ data_deaths <- rbind(deaths_all, deaths_age, deaths_sex, deaths_dep) %>%
 data_deaths %<>%
   pivot_wider(id_cols = c(area_type, category, type, week_ending), 
               names_from = area_name, values_from = count, values_fill = list(count = 0)) %>% 
-  pivot_longer(c(Scotland:`Western Isles`), values_to = "count", names_to = "area_name") %>% 
+  pivot_longer(c(`Aberdeen City`:`Western Isles`), values_to = "count", names_to = "area_name") %>% 
   # This is to get rid of combinations that don't exist (e.g. Scotland - Fife)
   mutate(area_id = paste(area_type, "-", area_name)) %>% 
   filter(area_id %in% unique(data_deaths$area_id)) %>% 
-  select(-area_id)
+  select(-area_id) %>% 
+  filter(area_name != "NHS Unknown residency")
 
 # Running final functions
 prepare_final_data(dataset = data_deaths, filename = "deaths", 
-                   last_week = "2021-02-21", aver = 5)
+                   last_week = "2021-03-28", aver = 5)
 
 ### END
