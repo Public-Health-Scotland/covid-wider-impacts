fedd21d09d91f5ba06f052a4ab54729519a4f886
Salomi Barkat <salomb01@nssrstudio03.csa.scot.nhs.uk>
2021-04-06 16:02:03 +0100

    Uncommenting so that credentials for updated immunisations PRA will be recognised

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -1,12 +1,12 @@
 #Server side
-# credentials <- readRDS("admin/credentials.rds")
+ credentials <- readRDS("admin/credentials.rds")
 
 function(input, output, session) {
   
  # Shinymanager Auth
-  # res_auth <- secure_server(
-  #   check_credentials = check_credentials(credentials)
-  # )
+   res_auth <- secure_server(
+     check_credentials = check_credentials(credentials)
+   )
   # 
   # output$auth_output <- renderPrint({
   #   reactiveValuesToList(res_auth)
