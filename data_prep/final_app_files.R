98b3160d33618aa79f982dbbeb159c3e0a35a03d
Laura Downie <laura.downie5@phs.scot#END>
2021-04-06 11:15:04 +0100

    GP OOH update

--- data_prep/final_app_files.R
+++ data_prep/final_app_files.R
@@ -128,6 +128,6 @@ save_final_file("mentalhealth_drugs", "01_Apr_21")
 ## Mental health - A&E ----
 save_final_file("mh_A&E", "01_Apr_21")
 ## Mental health - OOH ----
-save_final_file("mh_ooh", "01_Mar_21")
+save_final_file("mh_ooh", "06_Apr_21")
 
 ##END
