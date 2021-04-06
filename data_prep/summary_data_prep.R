62f8aacf69180e6da90557bcb337dfb640af7b41
Michael Webster <michaw01@nssrstudio07.csa.scot.nhs.uk>
2021-04-06 12:26:07 +0100

    rapid data update

--- data_prep/summary_data_prep.R
+++ data_prep/summary_data_prep.R
@@ -9,7 +9,7 @@ source("data_prep/functions_packages_data_prep.R")
 ## RAPID data ----
 ###############################################.
 # Prepared by Unscheduled care team
-rap_adm <- readRDS(paste0(data_folder, "rapid/Admissions_by_category_01-Mar.rds")) %>% 
+rap_adm <- readRDS(paste0(data_folder, "rapid/Admissions_by_category_06-Apr.rds")) %>% 
   janitor::clean_names() %>% 
   # taking out aggregated values, not clear right now
   filter(!(substr(hosp,3,5) == "All" | (substr(hscp_name,3,5) == "All")) &
@@ -99,7 +99,7 @@ rap_adm <- rbind(rap_adm, spec_med, paed_com) %>%
   # Excluding specialties groups with very few cases and of not much interest
   filter(!(spec %in% c("Dental", "Other"))) 
 
-prepare_final_data(rap_adm, "rapid", last_week = "2021-02-21", 
+prepare_final_data(rap_adm, "rapid", last_week = "2021-03-28", 
                    extra_vars = c("admission_type", "spec"))
 
 ###############################################.
