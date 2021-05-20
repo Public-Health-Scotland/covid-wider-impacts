5268a4cbb50a4df12d2818a8f09e6cc334f59969
Jaime Villacampa <jaime.villacampa@phs.scot>
2021-05-20 15:16:40 +0100

    schedule update

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -25,6 +25,7 @@ function(input, output, session) {
                    title = "New content added and future updates",
                    h4("New content and updates"),
                    tags$ul(
+                     tags$li("2nd June - Monthly update of data (most sections)."),
                      tags$li("5th May - Monthly update of data (most sections)."),
                      tags$li("14th April - New sections added for births and babies data:
                              Apgar scores and location of birth for extremely preterm infants."),
@@ -35,17 +36,14 @@ function(input, output, session) {
                    h4("Future updates"),
                    p("Please keep in mind that these dates are tentative and subject to change with short notice."),
                    tags$ul(
-                     tags$li("2nd June - Monthly update of data (most sections)."),
-                     tags$li("16 June 2021 - Outpatients data update."),
                      tags$li("16 June 2021 - Cancer pathology data update."),
+                     tags$li("16 June 2021 - Outpatients data update."),
                      tags$li("16 June 2021 - New data on vaginal tears."),
                      tags$li("7th July- Monthly update of data (most sections)."),
                      tags$li("June/July 2021 - New data on injury hospital admissions."),
-                     tags$li("June/July 2021 - New data on self-harm hospital admissions."),
+                     tags$li("July/August 2021 - New data on self-harm hospital admissions."),
                      tags$li("4th August - Monthly update of data (most sections)."),
                      tags$li("1st September - Monthly update of data (most sections).")
-                     
-                     
                    ),
                    size = "m",
                    easyClose = TRUE, fade=FALSE,footer = modalButton("Close (Esc)")))
