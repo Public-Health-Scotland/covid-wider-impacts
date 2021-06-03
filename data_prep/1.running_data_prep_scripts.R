6448408451690d6c17f2851182c41552ccbfd047
Jaime Villacampa <jaime.villacampa@phs.scot>
2021-06-03 15:01:59 +0100

    cardio prep functions

--- data_prep/1.running_data_prep_scripts.R
+++ data_prep/1.running_data_prep_scripts.R
@@ -1,11 +1,11 @@
 ## This code runs most scripts that produces data for the dashboard
+# The functions return an object per dataset, maybe this needs tweaked so it's a better one for checking
 
 ###############################################.
 ## Birth and babies datasets ----
 ###############################################.
 
 source("data_prep/births_babies_data_prep.R") # This sources the functions for the section
-#The function returns one object per dataset for checking purposes, maybe not the best one or more needed
 #This is perhaps a very big function and should be split into three (one for each delivery indicator)
 create_delivery(folderdate = "2021-05-18") # Mode of delivery, induction and gestation data
 create_perinatal(foldermonth = "june") # Stillbirths and perinatal mortality
@@ -13,8 +13,14 @@ create_apgar(folderdate = "2021_05_13") # Apgar scores
 create_preterm(preterm_date = "2021_03_18") # Apgar scores
 
 ###############################################.
-## Birth and babies datasets ----
+## Cardiovascular datasets ----
 ###############################################.
-
+source("data_prep/cardio_data_prep.R") # This sources the functions for the section
+create_aecardio(filedate = "2021-05-27", last_week =  "2021-05-23")
+create_oohcardio(filedate = "2021-05-31", last_week =  "2021-05-23")
+create_sascardio(filedate = "2021-05-31", last_week =  "2021-05-23")
+create_cardiodrugs(filedate = "2021-05-27", last_week =  "2021-05-23")
+# This function will need some work if we update again this dataset, but at the moment it hasn't happened
+create_cathlab()
 ##END
 
