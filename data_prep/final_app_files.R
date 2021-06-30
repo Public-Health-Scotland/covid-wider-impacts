9c299834aacc35e337c960b214f3e35f42d79f6d
Laura Downie <laura.downie5@phs.scot#END>
2021-06-30 14:53:34 +0100

    update finally complete!

--- data_prep/final_app_files.R
+++ data_prep/final_app_files.R
@@ -127,10 +127,10 @@ save_final_file("gestation_download_data", "28_Jun_21")
 ## B&B - Stillbirths and perinatal mortality ----
 save_final_file("perinatal", "19_May_21")
 ## B&B - Apgar ----
-save_final_file("apgar_runchart_data", "28_Jun_21")
-save_final_file("apgar_scot_data", "28_Jun_21")
-save_final_file("apgar_linechart_data", "28_Jun_21")
-save_final_file("apgar_download_data", "28_Jun_21")
+save_final_file("apgar_runchart_data", "30_Jun_21")
+save_final_file("apgar_scot_data", "30_Jun_21")
+save_final_file("apgar_linechart_data", "30_Jun_21")
+save_final_file("apgar_download_data", "30_Jun_21")
 ## B&B - Preterm ----
 save_final_file("preterm_linechart_data", "28_Jun_21")
 save_final_file("preterm", "28_Jun_21")
