6875f8ec82566e159c6b98464671a3286b14b417
murdoturner <murdo.turner@phs.scot>
2021-07-27 16:55:44 +0100

    Re-run sact data prep

--- data_prep/final_app_files.R
+++ data_prep/final_app_files.R
@@ -33,9 +33,9 @@ save_final_file("cancer_data_2", "27_Jul_21")
 
 ##########################################################.
 ## SACT ----
-save_final_file("sact_data", "26_Jul_21")
+save_final_file("sact_data", "27_Jul_21")
 
-save_final_file("sact_weekly_data", "26_Jul_21")
+save_final_file("sact_weekly_data", "27_Jul_21")
 
 ##########################################################.
 ## RAPID ----
