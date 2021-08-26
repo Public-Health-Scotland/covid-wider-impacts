45ecb0ecda9780c7f82faa1bdab54bf2cdac9a83
Neil Perkins <neilpe02@nssrstudio05.csa.scot.nhs.uk>
2021-08-26 13:23:54 +0100

    tweak to commentary

--- shiny_app/immunisation_tab.R
+++ shiny_app/immunisation_tab.R
@@ -639,6 +639,7 @@ output$immun_commentary_section <- renderUI({
     h2("Immunisations - 1st September 2021"),
     p("Information on the uptake of pre-school immunisations was updated in this tool on 1 September and includes information on cohorts eligible for their immunisations  
       to week beginning 5 July 2021."),
+    p("Background information on interpreting the data is provided in the commentary for previous updates below."),
     h2("Immunisations - 4th August 2021"),
     p("Information on the uptake of pre-school immunisations was updated in this tool on 4 August and includes information on cohorts eligible for their immunisations  
       to week beginning 7 June 2021."),
