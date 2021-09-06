07ae419429e77f585d84c64975dbf113fd3086a1
Sandra Storrie <sandrs01@nssrstudio07.csa.scot.nhs.uk>
2021-09-06 20:49:43 +0100

    chnaged the trend charts to account for monthly data

--- data_prep/functions_packages_data_prep.R
+++ data_prep/functions_packages_data_prep.R
@@ -187,7 +187,54 @@ prepare_final_data <- function(dataset, filename, last_week, extra_vars = NULL, 
   saveRDS(data_2020, paste0(open_data, filename,"_data.rds"))
 }
 
-
+# Function by month to format data in the right format for the Shiny app
+prepare_final_data_m <- function(dataset, filename, last_month, extra_vars = NULL, aver = 3) {
+  
+  # Creating week number to be able to compare pre-covid to covid period
+  dataset <- dataset %>% mutate(month_no = month(month_ending),
+                                # Fixing HSCP names
+                                area_name = gsub(" and ", " & ", area_name))
+  
+  if (aver == 3) {
+    # Creating average admissions of pre-covid data (2018-2019) by day of the year
+    historic_data <- dataset %>% filter(year(month_ending) %in% c("2018", "2019")) %>% 
+      group_by_at(c("category", "type", "area_name", "area_type", "month_no", extra_vars)) %>% 
+      # Not using mean to avoid issues with missing data for some weeks
+      summarise(count_average = round((sum(count, na.rm = T))/2, 1)) %>% 
+      ungroup()
+  }
+  # Joining with 2020 and 2021 data
+  # Filtering weeks with incomplete week too!! Temporary
+  data_2020 <- left_join(dataset %>% filter(year(month_ending) %in% c("2020", "2021")), 
+                         historic_data, 
+                         by = c("category", "type", "area_name", "area_type", "month_no", extra_vars)) %>% 
+    # Filtering cases without information on age, sex, area or deprivation (still counted in all)
+    filter(!(is.na(category) | category %in% c("Missing", "missing", "Not Known") |
+               is.na(area_name) | 
+               area_name %in% c("", "ENGLAND/WALES/NORTHERN IRELAND", "UNKNOWN HSCP - SCOTLAND",
+                                "ENGland/Wales/Northern Ireland", "NANA"))) %>% 
+    # Creating %variation from precovid to covid period 
+    mutate(count_average = ifelse(is.na(count_average), 0, count_average),
+           variation = round(-1 * ((count_average - count)/count_average * 100), 1),
+           # Dealing with infinite values from historic average = 0
+           variation =  ifelse(is.infinite(variation), 8000, variation)) %>% 
+    select(-month_no)
+  
+  if (aver == 3) {
+    # Supressing numbers under 5
+    data_2020 <- data_2020 %>% filter(count>=5) 
+  } 
+  
+  data_2020 <- data_2020 %>%
+    filter(month_ending <= as.Date(last_month))
+  
+  final_data <<- data_2020
+  
+  saveRDS(data_2020, paste0("shiny_app/data/", filename,".rds"))
+  saveRDS(data_2020, paste0(data_folder,"final_app_files/", filename, "_", 
+                            format(Sys.Date(), format = '%d_%b_%y'), ".rds"))
+  saveRDS(data_2020, paste0(open_data, filename,"_data.rds"))
+}
 # Function to format cardiac data in the right format for the Shiny app
 prepare_final_data_cardiac <- function(dataset, filename, last_week, extra_vars = NULL) {
   
