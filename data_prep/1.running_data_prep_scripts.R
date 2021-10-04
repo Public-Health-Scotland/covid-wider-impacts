0f4d9b17f265e0aaaf50a3d134dc1527b77791b1
ScottMG-PHS <scott.mclaren-gow@phs.scot>
2021-10-04 14:59:52 +0100

    Summary update Oct 21

--- data_prep/1.running_data_prep_scripts.R
+++ data_prep/1.running_data_prep_scripts.R
@@ -60,18 +60,18 @@ source("data_prep/summary_data_prep.R") # This sources the functions for the sec
 # Filedate: date on filename for each update, last week = last week of data to be included
 # Change extract to F if you just want to run the data prep and not the extraction 
 # (quicker once the extraction has been done once)
-create_rapid(last_week =  "2021-08-22", extract = T) # this requires access to the RAPID dataset
-create_ae(filedate = "2021-09-02", last_week =  "2021-08-22")
-create_ooh(filename = "WIDER IMPACT PC OOH Data_52_3329330824816909912", last_week = "2021-08-22")
-create_nhs24(filedate = "2021-08-30", last_week =  "2021-08-22")
-create_sas(filedate = "2021-08-30", last_week =  "2021-08-22")
+create_rapid(last_week =  "2021-09-26", extract = T) # this requires access to the RAPID dataset
+create_ae(filedate = "2021-09-30", last_week =  "2021-09-26")
+create_ooh(filename = "WIDER IMPACT PC OOH Data_53_9202834272022294897", last_week = "2021-09-26")
+create_nhs24(filedate = "2021-10-04", last_week =  "2021-09-26")
+create_sas(filedate = "2021-10-04", last_week =  "2021-09-26")
 
 # Deaths require access to deaths catalogue
 source("data_prep/deaths_data_preparation.R") # And the deaths function
 # Note: there will be a warning from match_area() about a few hundred thousand
 # geography codes not 9 characters in length - this is caused by the "Scotland"
 # rows, and is not a problem.
-create_deaths(last_week =  "2021-08-22")
+create_deaths(last_week =  "2021-09-26")
 
 ############## Remember to change final_app_files script dates
 # Then you need to change the update date in the summary_tab script 
