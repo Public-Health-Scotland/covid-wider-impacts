1c7cdfe5d345156468951b20598582792f59627f
murdoturner <murdo.turner@phs.scot>
2021-10-20 09:40:16 +0100

    Extract date corrected

--- shiny_app/global.R
+++ shiny_app/global.R
@@ -126,7 +126,7 @@ dce_data <- readRDS("data/dce_data.rds") %>%
   mutate(stage = factor(stage, levels = c("NK", "4", "3", "2", "1"), ordered = TRUE)) %>% 
   mutate(percent19 = as.numeric(percent19), percent20 = as.numeric(percent20))
 
-dce_extract_date <- "18 October 2021"
+dce_extract_date <- "8 October 2021"
 
 
 
