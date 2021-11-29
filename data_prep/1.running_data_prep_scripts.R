b925ec2313337c1758551f86369bd5c1e4fba176
ScottMG-PHS <scott.mclaren-gow@phs.scot>
2021-11-29 15:14:03 +0000

    rapid extract to TRUE

--- data_prep/1.running_data_prep_scripts.R
+++ data_prep/1.running_data_prep_scripts.R
@@ -60,7 +60,7 @@ source("data_prep/summary_data_prep.R") # This sources the functions for the sec
 # Filedate: date on filename for each update, last week = last week of data to be included
 # Change extract to F if you just want to run the data prep and not the extraction
 # (quicker once the extraction has been done once)
-create_rapid(last_week =  "2021-11-21", extract = F) # this requires access to the RAPID dataset
+create_rapid(last_week =  "2021-11-21", extract = T) # this requires access to the RAPID dataset
 create_ae(filedate = "2021-11-25", last_week =  "2021-11-21")
 create_ooh(filename = "WIDER IMPACT PC OOH Data_52_3843723932621185282", last_week = "2021-11-21")
 create_nhs24(filedate = "2021-11-29", last_week =  "2021-11-21")
