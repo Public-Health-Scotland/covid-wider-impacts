80a436f694d6f568befaf42ebccaf6b4a34f54b1
catherineperkins <catherine.perkins@phs.scot>
2021-11-29 11:49:39 +0000

    Dec21 update for cardio

--- data_prep/final_app_files.R
+++ data_prep/final_app_files.R
@@ -73,14 +73,14 @@ save_final_file("area_type_op", "24_Mar_21")
 ## Cardio - cath labs ----
 save_final_file("cath_lab", "16_Nov_20")
 ## Cardio - A&E ----
-save_final_file("ae_cardio", "01_Nov_21")
+save_final_file("ae_cardio", "29_Nov_21")
 save_final_file("ae_cardio_codes", "30_Nov_20")
 ## Cardio - OOH ----
-save_final_file("ooh_cardiac", "01_Nov_21")
+save_final_file("ooh_cardiac", "29_Nov_21")
 ## Cardio - SAS ----
-save_final_file("sas_cardiac", "01_Nov_21")
+save_final_file("sas_cardiac", "29_Nov_21")
 ## Cardio - Prescribing ----
-save_final_file("cardio_drugs", "01_Nov_21")
+save_final_file("cardio_drugs", "29_Nov_21")
 ##########################################################.
 ## Immunisations - six in one ----
 save_final_file("six_alldose", "27_Oct_21")
