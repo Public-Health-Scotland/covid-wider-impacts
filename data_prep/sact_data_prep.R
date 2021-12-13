163ae00875718930df9397d16a587a760fc89999
Tristram Gale <tristg01@nssrstudio02.csa.scot.nhs.uk>
2021-12-13 13:13:13 +0000

    SACT update

--- data_prep/sact_data_prep.R
+++ data_prep/sact_data_prep.R
@@ -21,7 +21,7 @@ require(lubridate)||install.packages("lubridate")
 
 year <- "2021"
 month <- "12"
-day <- "06"
+day <- "13"
 
 release_date <- paste(year, month, day, sep = "-")
 path_monthly_data <- paste0("////PHI_conf//CancerGroup1//Topics//CancerStatistics//Projects//20210205-SACT-dashboard//R Shiny//SACT Dashboard Data//SummaryMonthlyPatients-", release_date, ".csv")
