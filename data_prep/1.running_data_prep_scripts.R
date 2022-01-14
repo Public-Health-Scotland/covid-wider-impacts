38410822dcd137348aa02e326acfa0f94e66f1e7
Jaime Villacampa <jaime.villacampa@phs.scot>
2022-01-14 16:50:30 +0000

    refreshing to fix borders data

--- data_prep/1.running_data_prep_scripts.R
+++ data_prep/1.running_data_prep_scripts.R
@@ -61,7 +61,7 @@ source("data_prep/summary_data_prep.R") # This sources the functions for the sec
 # Change extract to F if you just want to run the data prep and not the extraction
 # (quicker once the extraction has been done once)
 create_rapid(last_week =  "2022-01-02", extract = T) # this requires access to the RAPID dataset
-create_ae(filedate = "2022-01-06", last_week =  "2022-01-02")
+create_ae(filedate = "2022-01-13", last_week =  "2022-01-02")
 create_ooh(filename = "WIDER IMPACT PC OOH Data_57_7301955574653426324", last_week = "2022-01-02")
 create_nhs24(filedate = "2022-01-10", last_week =  "2022-01-02")
 create_sas(filedate = "2021-11-29", last_week =  "2021-11-21")
