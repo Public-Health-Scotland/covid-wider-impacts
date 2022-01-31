9df829f1f57f530cda7fef5a21e009e91cf3b106
Sandra Storrie <sandrs01@nssrstudio02.csa.scot.nhs.uk>
2022-01-31 13:46:05 +0000

    Updated the dates on the final app files to todays date as thats the last time I ran the injuries_data_prep file. Thanks

--- data_prep/final_app_files.R
+++ data_prep/final_app_files.R
@@ -46,12 +46,12 @@ save_final_file("sact_weekly_data", "24_Jan_22")
 ##########################################################.
 ##########################################################.
 ## Injuries ----
-save_final_file("ui_smr01_all", "30_Sep_21")
-save_final_file("ui_smr01_rta", "30_Sep_21")
-save_final_file("ui_smr01_poison", "30_Sep_21")
-save_final_file("ui_smr01_other", "30_Sep_21")
-save_final_file("ui_smr01_falls", "30_Sep_21")
-save_final_file("ui_smr01_assaults", "30_Sep_21")
+save_final_file("ui_smr01_all", "01_Feb_22")
+save_final_file("ui_smr01_rta", "01_Feb_22")
+save_final_file("ui_smr01_poison", "01_Feb_22")
+save_final_file("ui_smr01_other", "01_Feb_22")
+save_final_file("ui_smr01_falls", "01_Feb_22")
+save_final_file("ui_smr01_assaults", "01_Feb_22")
 
 ##########################################################.
 ## RAPID ----
