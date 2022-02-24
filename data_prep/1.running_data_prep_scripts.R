fd93326e2c14499298bea0e8a53a1ecb9175b6ab
catherineperkins <catherine.perkins@phs.scot>
2022-02-24 15:23:56 +0000

    March22 update

--- data_prep/1.running_data_prep_scripts.R
+++ data_prep/1.running_data_prep_scripts.R
@@ -43,9 +43,9 @@ create_cathlab()
 ## Child health datasets ----
 ###############################################.
 source("data_prep/childhealth_data_prep.R") # This sources the functions for the section
-create_chreview(ch_date_file = "20220124") # Child health reviews. #date included in filepath name
-create_childdev(filedate = "24thJan2022")
-create_breastfeeding(filedate = "24thJan2022")
+create_chreview(ch_date_file = "20220221") # Child health reviews. #date included in filepath name
+create_childdev(filedate = "21stFeb2022")
+create_breastfeeding(filedate = "21stFeb2022")
 
 ############## Remember to change final_app_files script dates
 file.edit("data_prep/final_app_files.R")
