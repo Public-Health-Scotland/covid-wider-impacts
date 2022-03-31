5f4d5b46c14caae3c4f344850f4ad775ebfa258f
Neil Perkins <neilpe02@nssrstudio06.csa.scot.nhs.uk>
2022-03-31 09:37:01 +0100

    Updated text to remove broken links

--- shiny_app/immunisation_tab.R
+++ shiny_app/immunisation_tab.R
@@ -411,12 +411,9 @@ output$immunisation_explorer <- renderUI({
   
   
   # Intro paragraph within imumunisation tab
-  intro_6in1 <- p("Immunisation protects children against certain serious infections.  It is important that children ",
-                  tags$a(href="https://www.nhsinform.scot/illnesses-and-conditions/infections-and-poisoning/coronavirus-covid-19/healthy-living/coronavirus-covid-19-immunisation-and-screening",
-                         "continue to receive their routine immunisations during the Covid-19 pandemic", target="_blank"),".",
-                  "Public Health Scotland and Scottish Government have produced a range of communications reminding parents that the NHS is still open for childhood immunisations, signposting parents to up to date advice via ",
-                  tags$a(href="https://twitter.com/NHSImmuniseScot"," Immunise Scotland ", target="_blank"),
-                  " and ",tags$a(href="https://www.nhsinform.scot/immunisation","NHS inform", target="_blank"),".")
+  intro_6in1 <- p("Immunisation protects children against certain serious infections.
+                  Public Health Scotland and Scottish Government have produced a range of communications reminding parents that the NHS is still open for childhood immunisations, signposting parents to up to date advice via ",
+                 ,tags$a(href="https://www.nhsinform.scot/immunisation","NHS inform", target="_blank"),".")
   
   #Additional commentart/meta data to appear on immunisation tab
   commentary_6in1 <-  tagList(p("All preschool children are offered a total of five immunisation appointments as they reach the following ages: 8, 12, and 16 weeks; 12-13 months; and 3 years and 4 months of age. Multiple immunisations are offered at each appointment. Here, for simplicity, we have just shown the uptake of one of the immunisations offered at each appointment. The charts show the progression of uptake of the relevant immunisation as children age and the data tables provide the uptake rates at three specific time-points.  Data is provided  on children who have become eligible for immunisation during the pandemic (from March 2020 onwards) and for children who became eligible for immunisation before the pandemic (in 2019 and in January and February 2020) for comparison."),
