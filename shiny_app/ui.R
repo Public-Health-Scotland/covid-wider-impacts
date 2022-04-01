0480229beb682cfc05b83ae33b8728c62515c1b9
Jaime Villacampa <jaime.villacampa@phs.scot>
2022-04-01 10:04:45 +0100

    dce commented out

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -363,72 +363,72 @@ tabPanel(title = "Cardiovascular", icon = icon("heartbeat"), value = "cardio",
                                  mainPanel(width = 12,
                                            uiOutput("sact_wk_explorer")
                                  )# mainPanel bracket
-                              ), # tabpanel bracket
+                              )#, # tabpanel bracket
 
                         ###############################################.
                         ## DCE ----
                         ###############################################.
                         
-                        tabPanel(title = "Cancer Staging - DCE Data", icon = icon("clock"), value = "dce",
-                                 wellPanel(h4(strong("Cancer Staging - Detect Cancer Early Data (Breast, Colorectal & Lung)")),
-                                           # p("Cancer is one of the major causes of death in Scotland. In 2018, 16,153 people died of cancer 
-                                           #    in Scotland and approximately 34,000 people were diagnosed with cancer, excluding non-melanoma 
-                                           #      skin cancer. The most common causes of cancer diagnosis are lung, breast, prostate and colorectal cancer."),
-                                           # p("In February 2012 the Cabinet Secretary for Health and Wellbeing formally launched the Detect Cancer Early 
-                                           #    programme . One aim of the Detect Cancer Early programme was to increase the proportion of people who were 
-                                           #    diagnosed early in the disease process (with stage 1 disease). The programme concentrates on breast, colorectal 
-                                           #    and lung cancers, which collectively account for 42.6% of all cancers diagnosed in Scotland in 2018."),
-                                           p("Cancer staging is the process of determining the extent to which a cancer has developed and spread. 
-                                             For the majority of patients with cancer it is common practice to assign a number from 1 to 4 to a cancer, 
-                                             with 1 indicating the cancer is confined to the original organ in which it occurred and 4 being a cancer 
-                                             which has spread beyond the original organ and its local lymph glands (regional lymph nodes). Patients 
-                                             diagnosed with stage 1 disease tend to have better outcomes and longer survival compared with patients 
-                                             diagnosed with stage 4 disease."),
-                                           p("This dashboard looks at breast, colorectal and lung cancer data separately to examine the 
-                                             different changes on stage at diagnosis from the year the coronavirus pandemic began compared with 2019."),
-                                           p("The proportions of patients with any given cancer stage may be affected by a number of things, including 
-                                             changes in the proportions of other stages (including those that are not known).  These data can only 
-                                             describe patients who were diagnosed with cancer and in a separate section of this dashboard, it is estimated 
-                                             that total breast, colorectal and lung cancer diagnoses fell by 16%, 21% and 21%, respectively, in 2020 
-                                             compared with 2019. Temporary pausing of the national screening programmes for breast and colorectal cancer 
-                                             in 2020 is likely to have particularly reduced numbers of early stage cancers being diagnosed.  A full 
-                                             understanding of the various determinants of any changes in stage of cancer when it was diagnosed after 
-                                             the pandemic began, and of the status of the people who were not diagnosed with cancer as expected in 2020, 
-                                             will take time to be reached."),
-                                           # tags$ul(
-                                           #   tags$li("For breast cancer, there were large falls numbers in stages 1 and 2 (35% and 15% respectively). In 
-                                           #           contrast, there were small increases in stages 3 and 4 (5% and 7%), with the biggest increase seen for 
-                                           #           those of unknown stage (34%)."),
-                                           #   
-                                           #   tags$li("For Colorectal Cancer, there were substantial drops (30% and more) in the numbers diagnosed with 
-                                           #           stages 1, 2 or 3 colorectal cancer; whereas there was only a 4% drop for metastatic colorectal cancer."),
-                                           #   
-                                           #   tags$li("For Lung Cancer, there were falls of 11%-13% for stages 1, 2 and 3; but only a fall of 4% for stage 4 
-                                           #           diagnoses, which was only lower than expected in April 2020.")),
-                                           
-                                           p(strong(paste0("Figures presented based on data extracted on ",dce_extract_date)))
-                                           ),
-                                 wellPanel(
-                                   column(5, selectInput("geotype_dce", label = "Select a geography level",
-                                                         choices= c("Scotland", "Cancer Network"),
-                                                         selected = "Scotland"),
-                                          uiOutput("geoname_ui_dce")),
-                                   column(5,  selectInput("dce_type", label = "Select all or specific cancer type",
-                                                          choices = c("Breast", "Colorectal", "Lung"), selected = "Breast")),
-                                   column(2,
-                                          fluidRow(br()),
-                                          actionButton("btn_dce_modal", "Data source: ", icon = icon('question-circle')),
-                                          fluidRow(br()),
-                                          downloadButton('download_dce_data', 'Download data')) #,
-                                 ) , #well panel
-                                 mainPanel(width = 12,
-                                           uiOutput("dce_explorer1") ,
-                                           div(radioButtons("dce_stage", "Select stage of cancer (NK - Not Known)",
-                                                            list("1","2","3","4","NK"), inline = TRUE,
-                                                            selected = "1")),
-                                           uiOutput("dce_explorer2")
-                                 )# mainPanel bracket
-                              ) # tabpanel bracket
+                        # tabPanel(title = "Cancer Staging - DCE Data", icon = icon("clock"), value = "dce",
+                        #          wellPanel(h4(strong("Cancer Staging - Detect Cancer Early Data (Breast, Colorectal & Lung)")),
+                        #                    # p("Cancer is one of the major causes of death in Scotland. In 2018, 16,153 people died of cancer 
+                        #                    #    in Scotland and approximately 34,000 people were diagnosed with cancer, excluding non-melanoma 
+                        #                    #      skin cancer. The most common causes of cancer diagnosis are lung, breast, prostate and colorectal cancer."),
+                        #                    # p("In February 2012 the Cabinet Secretary for Health and Wellbeing formally launched the Detect Cancer Early 
+                        #                    #    programme . One aim of the Detect Cancer Early programme was to increase the proportion of people who were 
+                        #                    #    diagnosed early in the disease process (with stage 1 disease). The programme concentrates on breast, colorectal 
+                        #                    #    and lung cancers, which collectively account for 42.6% of all cancers diagnosed in Scotland in 2018."),
+                        #                    p("Cancer staging is the process of determining the extent to which a cancer has developed and spread. 
+                        #                      For the majority of patients with cancer it is common practice to assign a number from 1 to 4 to a cancer, 
+                        #                      with 1 indicating the cancer is confined to the original organ in which it occurred and 4 being a cancer 
+                        #                      which has spread beyond the original organ and its local lymph glands (regional lymph nodes). Patients 
+                        #                      diagnosed with stage 1 disease tend to have better outcomes and longer survival compared with patients 
+                        #                      diagnosed with stage 4 disease."),
+                        #                    p("This dashboard looks at breast, colorectal and lung cancer data separately to examine the 
+                        #                      different changes on stage at diagnosis from the year the coronavirus pandemic began compared with 2019."),
+                        #                    p("The proportions of patients with any given cancer stage may be affected by a number of things, including 
+                        #                      changes in the proportions of other stages (including those that are not known).  These data can only 
+                        #                      describe patients who were diagnosed with cancer and in a separate section of this dashboard, it is estimated 
+                        #                      that total breast, colorectal and lung cancer diagnoses fell by 16%, 21% and 21%, respectively, in 2020 
+                        #                      compared with 2019. Temporary pausing of the national screening programmes for breast and colorectal cancer 
+                        #                      in 2020 is likely to have particularly reduced numbers of early stage cancers being diagnosed.  A full 
+                        #                      understanding of the various determinants of any changes in stage of cancer when it was diagnosed after 
+                        #                      the pandemic began, and of the status of the people who were not diagnosed with cancer as expected in 2020, 
+                        #                      will take time to be reached."),
+                        #                    # tags$ul(
+                        #                    #   tags$li("For breast cancer, there were large falls numbers in stages 1 and 2 (35% and 15% respectively). In 
+                        #                    #           contrast, there were small increases in stages 3 and 4 (5% and 7%), with the biggest increase seen for 
+                        #                    #           those of unknown stage (34%)."),
+                        #                    #   
+                        #                    #   tags$li("For Colorectal Cancer, there were substantial drops (30% and more) in the numbers diagnosed with 
+                        #                    #           stages 1, 2 or 3 colorectal cancer; whereas there was only a 4% drop for metastatic colorectal cancer."),
+                        #                    #   
+                        #                    #   tags$li("For Lung Cancer, there were falls of 11%-13% for stages 1, 2 and 3; but only a fall of 4% for stage 4 
+                        #                    #           diagnoses, which was only lower than expected in April 2020.")),
+                        #                    
+                        #                    p(strong(paste0("Figures presented based on data extracted on ",dce_extract_date)))
+                        #                    ),
+                        #          wellPanel(
+                        #            column(5, selectInput("geotype_dce", label = "Select a geography level",
+                        #                                  choices= c("Scotland", "Cancer Network"),
+                        #                                  selected = "Scotland"),
+                        #                   uiOutput("geoname_ui_dce")),
+                        #            column(5,  selectInput("dce_type", label = "Select all or specific cancer type",
+                        #                                   choices = c("Breast", "Colorectal", "Lung"), selected = "Breast")),
+                        #            column(2,
+                        #                   fluidRow(br()),
+                        #                   actionButton("btn_dce_modal", "Data source: ", icon = icon('question-circle')),
+                        #                   fluidRow(br()),
+                        #                   downloadButton('download_dce_data', 'Download data')) #,
+                        #          ) , #well panel
+                        #          mainPanel(width = 12,
+                        #                    uiOutput("dce_explorer1") ,
+                        #                    div(radioButtons("dce_stage", "Select stage of cancer (NK - Not Known)",
+                        #                                     list("1","2","3","4","NK"), inline = TRUE,
+                        #                                     selected = "1")),
+                        #                    uiOutput("dce_explorer2")
+                        #          )# mainPanel bracket
+                        #       ) # tabpanel bracket
                         )  , # navbar bracket          
 
 ###############################################.
