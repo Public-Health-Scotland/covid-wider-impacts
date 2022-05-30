87ff274bf6e898ead7b1ad4e5d41aafe815a25e4
michaelnisbett <michael.nisbett@nhs.net>
2022-05-30 11:13:44 +0100

    updated for latest month

--- data_prep/final_app_files.R
+++ data_prep/final_app_files.R
@@ -100,7 +100,7 @@ save_final_file("age_defs_imm_6inone", "27_Apr_22")
 save_final_file("age_defs_imm_mmr", "27_Apr_22")
 save_final_file("month_eligibility_immun", "27_Apr_22")
 ##########################################################.
-ch_date <- "28_Apr_22"
+ch_date <- "27_May_22"
 ## Child health reviews - first visit ----
 save_final_file("first_visit", ch_date)
 save_final_file("first_visit_datatable", ch_date)
