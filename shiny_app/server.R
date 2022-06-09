207d67809c0a9c70b75c438418fd07ecad5ccf11
Jaime Villacampa <jaime.villacampa@phs.scot>
2022-06-09 13:09:33 +0100

    schedule

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -25,23 +25,19 @@ function(input, output, session) {
                  title = "New content added and future updates",
                  h4("New content and updates"),
                  tags$ul(
-                   tags$li("8 June 2022 - Systemic anti-cancer therapies data update."),
+                   tags$li("15 June 2022 - Systemic anti-cancer therapies data update."),
+                   tags$li("15 June 2022 - Outpatient activity data update."),
                    tags$li("1 June 2022 - Monthly update of data (most sections)."),
                    tags$li("25 May 2022 - Additional data for the cardiovascular section: hospital admissions and excess mortality."),
                    tags$li("25 May 2022 - Cancer pathology data update."),
-                   tags$li("4 May 2022 - Monthly update of data (most sections)."),
-                   tags$li("6 April 2022 - Monthly update of data (most sections)."),
                    tags$li("30 March 2022 - New data on ambulance administered naloxone and update of substance use section."),
-                   tags$li("9 March 2022 - Outpatient activity data update."),
-                   tags$li("2 March 2022 - Monthly update of data (most sections)."),
                    tags$li("16 February 2022 - Adding ethnicity data to the outpatients activity dataset."),
                    tags$li("2 February 2022 - Improvements to visualisations in pregnancy, births and babies, and child health section.")
                    ),
                  h4("Future updates"),
                  p("Please keep in mind that these dates are tentative and subject to change with short notice."),
                  tags$ul(
-                   tags$li("15 June 2022 - Systemic anti-cancer therapies data update."),
-                   tags$li("15 June 2022 - Outpatient activity data update."),
+                   tags$li("22 June 2022 - Systemic anti-cancer therapies data update."),
                    tags$li("6 July 2022 - Monthly update of data (most sections)."),
                    tags$li("3 August 2022 - Monthly update of data (most sections)."),
                    tags$li("TBC 2022 - Cancer staging data.")
