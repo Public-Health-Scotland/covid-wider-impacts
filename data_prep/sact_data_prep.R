57cdb6444045e3e21267204ab7ca3795c761f2bb
Tristram Gale <tristg01@nssrstudio02.csa.scot.nhs.uk>
2022-07-04 10:24:23 +0100

    SACT update (server dates already changed)

--- data_prep/sact_data_prep.R
+++ data_prep/sact_data_prep.R
@@ -20,8 +20,8 @@ require(lubridate)||install.packages("lubridate")
 # Date Input (Monday of data refresh)
 
 year <- "2022"
-month <- "06"
-day <- "27"
+month <- "07"
+day <- "04"
 
 release_date <- paste(year, month, day, sep = "-")
 path_monthly_data <- paste0("////PHI_conf//SCRIS//SACT//Projects//20210205-SACT-dashboard//R Shiny//SACT Dashboard Data//SummaryMonthlyPatients-", release_date, ".csv")
