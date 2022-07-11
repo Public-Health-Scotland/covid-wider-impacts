cc9509bcdbfb8ec7129dd8a2275773e2410de0a3
Isabella Tortora <isabella.tortorabraydadibelvedere@phs.scot>
2022-07-11 13:34:55 +0100

    Fixed a couple of postcodes that were excluded incorrectly.

--- data_prep/cancer_data_prep.R
+++ data_prep/cancer_data_prep.R
@@ -271,10 +271,21 @@ cancer <- cancer %>%
       paste0(substr(postcode, 1, 2), substr(postcode, 4, 8)),
     TRUE ~ postcode))
 
+## also correct: - instances with 1 as the first character where it should be an I.
+# - instances where the second to last letter is a 0 (instead of O)
+
+
+cancer <- cancer %>% 
+  mutate(postcode = case_when(
+    substr(postcode, 1,1) == "1" ~ sub("^.", "I", postcode),
+    TRUE ~ postcode
+  ))
+
 # Convert 8-character postcodes to 7-character postcodes.
 # 'pc7' always assumes a 7 character length and there could be zero, one or two spaces 
 # between Postcode District and Postcode Sector / Walk.
-
+# - instances where a 3_3 postcode is 4_2,
+# - instances where a 3_3 postcode is 3_1_2
 
 cancer <- cancer %>%
   mutate(postcode = case_when(
@@ -282,13 +293,15 @@ cancer <- cancer %>%
     str_length(postcode) == 7 ~ postcode,
     str_length(postcode) == 6 ~ paste0(substr(postcode, 1, 3), ' ', substr(postcode, 4, 6)),
     str_length(postcode) == 5 ~ paste0(substr(postcode, 1, 2), '  ', substr(postcode, 3, 5)),
+    (str_length(postcode) == 8 & substr(postcode,4,4) == " " & substr(postcode, 6,6) == " ") ~ 
+       paste0(substr(postcode,1,3), ' ',substr(4,6)),
     TRUE ~ postcode))
 
 # VARIABLE LABELS pc7 'Postcode of Residence (7 character format)'.
 #
 # 'pc8' works in a strange way, always assuming only one space between Postcode District and 
 # Postcode Sector / Walk.
-#
+
 cancer <- cancer %>%
   mutate(postcode = case_when(
     substr(postcode, 5, 5) == 'O' ~ paste0(substr(postcode, 1, 4), '0', substring(postcode, 6)),
@@ -298,7 +311,7 @@ cancer <- cancer %>%
 #
 # VARIABLE LABELS pc8 'Postcode of Residence (8 character format)'.
 
-cancer <- cancer %>%
+cancer1 <- cancer %>%
   mutate(postcode = case_when(
     substr(postcode, 4, 4) != ' ' & substr(postcode, 5, 5) != ' ' & substr(postcode,7,7) != ' ' ~
       paste0(substr(postcode,1,4), " ", substr(postcode, 5, 7)),
@@ -306,12 +319,18 @@ cancer <- cancer %>%
   mutate(postcode = case_when(
     substr(postcode, 3, 4) == '  ' & substr(postcode, 5, 5) != ' ' & substr(postcode,7,7) != ' ' ~
       paste0(substr(postcode,1,2), " ", substr(postcode, 5, 7)),
-    TRUE ~ postcode))
+    TRUE ~ postcode)) 
 
 #format postcode to 8 digit
-cancer <- cancer %>%
+cancer <- cancer1 %>%
   mutate(postcode = format_postcode(postcode, format = "pc8"))
 
+## A few instances where format_postcode incorrectly excludes individuals. Fix these tomorrow... 
+
+# find records dropped in the above line of code:
+test<-anti_join(cancer,cancer1)
+
+## A few more postcodes to be fixed:
 ### get Health Boards of residence and deprivation quintile rank from postcodes
 
 cancer <- cancer %>% 
