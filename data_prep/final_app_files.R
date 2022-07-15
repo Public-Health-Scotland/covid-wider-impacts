7cccccea19b95a8fa0d53478eaa2c60b9eef2368
Isabella Tortora <isabella.tortorabraydadibelvedere@phs.scot>
2022-07-15 11:48:33 +0100

    Updated final file date.

--- data_prep/final_app_files.R
+++ data_prep/final_app_files.R
@@ -29,9 +29,9 @@ save_final_file <- function(dataset, date_saved) {
 save_final_file("deaths", "30_May_22")
 ##########################################################.
 ## Cancer ----#
-save_final_file("cancer_data_2", "27_Jun_22")
-save_final_file("cancer_data_quarters", "27_Jun_22")
-save_final_file("cancer_data_quarters_2yr", "27_Jun_22")
+save_final_file("cancer_data_2", "15_Jul_22")
+save_final_file("cancer_data_quarters", "15_Jul_22")
+save_final_file("cancer_data_quarters_2yr", "15_Jul_22")
 
 
 ##########################################################.
