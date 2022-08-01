b1ebe0fa73ed1b2b4b6d17776838577faa7bbce9
catherineperkins <catherine.perkins@phs.scot>
2022-08-01 16:37:42 +0100

    Changed the order of filters in Summary trends

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -317,32 +317,31 @@ tabPanel(title = "Commentary", icon = icon("list-ul"), value = "comment",
 #############################################.
 tabPanel(title = "Summary trends", icon = icon("area-chart"), value = "summary",
   wellPanel(#actionButton("browser", "browser"),
-    column(4,
-           conditionalPanel(condition = "input.measure_select != 'outpats' ",
-           div(title="Select a geography level first, then select the are you want from the list. You can click in the box, hit backspace and start to type if you want to start searching.",
-                  
-                  selectInput("geotype", 
-                              label = "Step 1. Select a geography level and then an area of interest.",
-                              choices= c("Scotland", "Health board", "HSC partnership"),
-                              selected = "Scotland")),
-           uiOutput("geoname_ui")),
+    column(3, div(title="Select the data you want to explore.", # tooltip
+                                          selectizeInput("measure_select",
+                                                         label= "Step 1. Select the data you want to explore.",
+                                                         choices = data_list, selected = "Hospital admissions",
+                                                         multiple = F))),
+    conditionalPanel(condition = "input.measure_select != 'outpats' ",
+                            column(4, div(title="Select a geography level first, then select the are you want from the 
+                                          list. You can click in the box, hit backspace and start to type if you want to start searching.",
+                                          selectInput("geotype", 
+                                                      label = "Step 2. Select a geography level and then an area of interest.",
+                                                      choices= c("Scotland", "Health board", "HSC partnership"),
+                                                      selected = "Scotland")),
+                                   uiOutput("geoname_ui"))),
             # If outpatients selected bring other set of choices
            conditionalPanel(condition = "input.measure_select == 'outpats' ",
-                            div(title="Select a geography level first, then select the area you want from the list. You can click in the box, hit backspace and start to type if you want to start searching.",
 
-                                selectInput("geotype_op", 
-                                            label = "Step 1. Select a geography level and then an area of interest.",
+                            column(4, div(title="Select a geography level first, then select the area you want from the list. You can click in the 
+                                box, hit backspace and start to type if you want to start searching.",
+                                          selectInput("geotype_op", 
+                                            label = "Step 2. Select a geography level and then an area of interest.",
                                             choices= c("Scotland", "Health board of treatment",
                                                        "Health board of residence",
                                                        "HSC partnership of residence"),
                                             selected = "Scotland")),
-                            uiOutput("geoname_op_ui"))
-           ),
-    column(3, div(title="Select the data you want to explore.", # tooltip
-                  selectizeInput("measure_select",
-                          label= "Step 2. Select the data you want to explore.",
-                          choices = data_list, selected = "Hospital admissions",
-                          multiple = F))),
+                            uiOutput("geoname_op_ui"))),
 
     column(3,
            conditionalPanel(condition = "input.measure_select != 'outpats' ",
@@ -352,7 +351,7 @@ tabPanel(title = "Summary trends", icon = icon("area-chart"), value = "summary",
                             selectInput("ooh_appt_type", label = "Step 4. Select type of appointment for overall chart.",
                                         choices = c("All cases", "All consultations" = "ALL",
                                                     "Covid consultations" = "COVID", "Non-covid consultations" = "NON COVID"),
-                                        selected = "All Cases")),
+                                      selected = "All Cases")),
            conditionalPanel(condition = "input.measure_select == 'outpats' ",
                             selectInput("appt_type", label = "Step 3. Select type of appointment.",
                                         choices = c("All", "New", "Return"), selected = "All")),
@@ -360,7 +359,7 @@ tabPanel(title = "Summary trends", icon = icon("area-chart"), value = "summary",
                             selectInput("time_type", label = "Step 4. Select weekly or monthly data.",
                                         choices = c("Weekly", "Monthly"), selected = "Weekly"))),
     column(2,
-           downloadButton('download_chart_data', 'Download data'),
+           downloadButton('download_chart_data', 'Download data'), br(), br(),
            actionButton('jump_commentary_summary','Go to commentary'))
     
    ), #wellPanel bracket
@@ -376,7 +375,7 @@ tabPanel(title = "Cardiovascular", icon = icon("heartbeat"), value = "cardio",
          wellPanel(
            column(4, div(title="Select the data you want to explore.", # tooltip
                          radioGroupButtons("measure_cardio_select",
-                                           label= "Step 1 – Select the data you want to explore.",
+                                           label= "Step 1. Select the data you want to explore.",
                                            choices = cardio_list, status = "primary",
                                            direction = "vertical", justified = T))),
            column(4, selectizeInput("area_cardio_select", "Step 2 - Select the area of interest",
