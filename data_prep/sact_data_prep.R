d184a8e7bb150efe97784c30a5f5d8fa15c99ebc
Tristram Gale <tristg01@nssrstudio05.csa.scot.nhs.uk>
2022-08-15 12:22:25 +0100

    SACT update

--- data_prep/sact_data_prep.R
+++ data_prep/sact_data_prep.R
@@ -21,7 +21,7 @@ require(lubridate)||install.packages("lubridate")
 
 year <- "2022"
 month <- "08"
-day <- "08"
+day <- "15"
 
 release_date <- paste(year, month, day, sep = "-")
 path_monthly_data <- paste0("////PHI_conf//SCRIS//SACT//Projects//20210205-SACT-dashboard//R Shiny//SACT Dashboard Data//SummaryMonthlyPatients-", release_date, ".csv")
